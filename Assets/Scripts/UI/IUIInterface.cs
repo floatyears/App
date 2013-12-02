05b01df724e660502ac19c645e7a9e98a5074682
CodeSuperHero <303040565@qq.com>
2013-12-02 13:35:38 +0800

    no message

---
+++ Assets/Scripts/UI/IUIInterface.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IUIInterface 
+{
+	/// <summary>
+	/// ui name
+	/// </summary>
+	/// <value>The name.</value>
+	string name{set;get;}
+
+	/// <summary>
+	/// parent object
+	/// </summary>
+	GameObject UIRoot{set;}
+
+
+	UIManager uiManager{set;}
+	/// <summary>
+	/// creat ui object
+	/// </summary>
+	/// <param name="UIRoot">User interface root.</param>
+	void CreatUI(GameObject UIRoot);
+
+	/// <summary>
+	/// show ui object on screen
+	/// </summary>
+	void ShowUI();
+
+	/// <summary>
+	/// hide ui object
+	/// </summary>
+	void HideUI();
+
+	/// <summary>
+	/// destory ui object
+	/// </summary>
+	void DestoryUI();
+	
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIManager
+{
+
+	private static UIManager instance;
+
+	/// <summary>
+	/// singleton 
+	/// </summary>
+	/// <value>The instance.</value>
+	public static UIManager Instance
+	{
+		get
+		{
+			if(instance  == null)
+				instance = new UIManager();
+
+			return instance;
+		}
+	}
+
+
+	#region UI object manager
+	private Dictionary<string,IUIInterface> uiDic = new Dictionary<string, IUIInterface>();
+
+	/// <summary>
+	/// add ui to uimanager
+	/// </summary>
+	/// <param name="uiName">ui name.</param>
+	/// <param name="ui">ui object.</param>
+	public void AddUIObject(string uiName,IUIInterface ui)
+	{
+		if(!uiDic.ContainsKey(uiName))
+			uiDic.Add(uiName,ui);
+		else
+			uiDic[uiName] = ui;
+	}
+
+	/// <summary>
+	/// have this ui object?
+	/// </summary>
+	/// <returns><c>true</c> if this instance has user interface object the specified uiName; otherwise, <c>false</c>.</returns>
+	/// <param name="uiName">ui name.</param>
+	public bool HasUIObject(string uiName)
+	{
+		if(uiDic.ContainsKey(uiName))
+			return true;
+
+		return false;
+	}
+
+	/// <summary>
+	/// get this ui object
+	/// </summary>
+	/// <returns>ui object.</returns>
+	/// <param name="uiName">ui name.</param>
+	public IUIInterface GetUI(string uiName)
+	{
+		if(uiDic.ContainsKey(uiName))
+			return uiDic[uiName];
+		else
+			return null;
+	}
+
+	/// <summary>
+	/// Remove UI
+	/// </summary>
+	/// <param name="uiName">User interface name.</param>
+	public void RemoveUI(string uiName)
+	{
+		if(uiDic.ContainsKey(uiName))
+			uiDic.Remove(uiName);
+	}
+	#endregion
+
+
+}
