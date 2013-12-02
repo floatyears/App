d83b162ef524d414b460dcaad855dc5de87291bd
PLY <lingyan@big-bang.com>
2013-12-02 20:35:12 +0800

    no message

---
+++ Assets/Scripts/Fight/Card.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour 
+{
+	public bool canMove = true;
+	public enum Styles
+	{
+		wind,
+		fire,
+		water,
+		light
+	}
+	public Styles style;
+}
