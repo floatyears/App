66b731af7e3536990b45a5b512fef80b8ac5672d
CodeSuperHero <303040565@qq.com>
2013-12-04 19:44:45 +0800

    no message

---
+++ Assets/Main.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using ProtoBuf;
+
+public class Main : MonoBehaviour 
+{
+	public GameObject uiRoot;
+	
+	/// <summary>
+	/// start game
+	/// </summary>
+	void OnEnable()
+	{
+		ControllerManager.Instance.Init(uiRoot);
+		ControllerManager.Instance.ChangeScene(SceneEnum.Quest);
+
+		//FileStream fs = new FileStream((Application.dataPath + "/Scripts/Protobuf/Person.proto"),FileMode.Open,FileAccess.Read);
+
+		//ProtoReader pr=  new ProtoReader(fs,ProtoBuf.Meta.TypeModel.SerializeType,
+//		msg.Person person = new msg.Person();
+//
+//		person.name = "aaa";
+//		person.id = 11;
+//
+//		ProtoBuf.Serializers.
+
+	}
+}
