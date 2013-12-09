7fd60bf1a3a2db63b0fecdcbc7127d20bfda2239
CodeSuperHero <303040565@qq.com>
2013-12-09 17:53:07 +0800

    no message

---
+++ Assets/Scripts/Utility/Config.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Config 
+{
+	private static Config instance;
+
+	public static Config Instance
+	{
+		get
+		{
+			if(instance == null)
+				instance = new Config();
+
+			return instance;
+		}
+	}
+
+	public const byte cardPoolSingle = 5;
+
+	private Dictionary<int,ItemData> cardData = new Dictionary<int, ItemData>();
+
+	public Dictionary<int,ItemData> CardData
+	{
+		get{return cardData;}
+	}
+
+	private Config()
+	{
+		ItemData cid;
+		for (int i = 0; i < 4; i++) 
+		{
+			int j = i +1;
+			cid = new ItemData(j,"Card"+j,1);
+			cardData.Add(cid.itemID,cid);
+		}
+	}
+}
+
+public class ItemData
+{
+	public int itemID;
+
+	public string itemName;
+
+	public ResourceEuum resourceEnum;
+
+	public ItemData(int ID,string name,byte type)
+	{
+		this.itemID = ID;
+		this.itemName = name;
+
+		resourceEnum = (ResourceEuum)type;
+	}
+
+}
---
+++ Assets/Scripts/View/Battle/CardPoolSingleItem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardPoolItem : UIBaseUnity 
+{
+	private CardPoolEnum cardPoolType;
+
+	public CardPoolEnum CardPoolType
+	{
+		set{cardPoolType = value;}
+		get{return cardPoolType;}
+	}
+
+
+	private UITexture uiTexture;
+
+	private UITexture itemTexture;
+
+	public override void Init (string name)
+	{
+		base.Init (name);
+
+		uiTexture = FindChild<UITexture>("Back");
+
+		itemTexture = FindChild<UITexture>("Texture");
+	}
+
+	void GenerateCard(int cardID)
+	{
+		Vector3 initPosition = transform.localPosition;
+
+		float width = uiTexture.mainTexture.width;
+
+		float height = uiTexture.mainTexture.height;
+
+		Texture2D tex2d = LoadAsset.Instance.LoadAssetFromResource(cardID) as Texture2D;
+
+		if(!itemTexture.enabled)
+			itemTexture.enabled = true;
+
+		itemTexture.mainTexture = tex2d;
+
+	}
+
+}
