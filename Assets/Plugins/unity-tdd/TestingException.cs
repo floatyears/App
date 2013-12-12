d2aef4eae78ad7afe0a143a99766c4ed376f5f24
yuechuzhao <oisiiryouri@outlook.com>
2013-12-12 12:56:48 +0800

    1.add unity-tdd 2.add sqlite support

---
+++ Assets/Plugins/unity-tdd/TestingException.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+namespace UnitTesting
+{
+	[System.Serializable]
+	public class TestingException : System.Exception
+	{
+		public string rawMessage { get; private set; }
+
+		public TestingException() : base() { }
+
+		public TestingException(string message) : base(message)
+		{
+			rawMessage = message;
+		}
+
+		public TestingException(string message, System.Exception inner) : base(message, inner)
+		{
+			rawMessage = message;
+		}
+	}
+}
+#endif
