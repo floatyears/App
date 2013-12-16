dcd41cdd76bef927c5a1d10ff52a4bc300cab487
CodeSuperHero <303040565@qq.com>
2013-12-16 19:36:08 +0800

    map config class

---
+++ Assets/Scripts/Model/Map/MapConfig.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapConfig 
+{
+	public int mapXLength;
+	public int mapYLength;
+	
+	public SingleMapData[,] mapData;
+	
+	public int characterInitCoorX = 2;
+	public int characterInitCoorY = 0;
+	
+	public MapConfig ()
+	{
+		mapXLength = 5;
+		mapYLength = 5;
+		
+		mapData = new SingleMapData[mapXLength,mapYLength];
+
+		for (int i = 0; i < mapXLength; i++) 
+		{
+			for (int j = 0; j < mapYLength; j++) 
+			{
+				SingleMapData smd = new SingleMapData();
+				smd.StarLevel = Random.Range(0,5);
+				smd.CoordinateX = i;
+				smd.CoordinateY = j;
+				
+				for (int k = 0; k < smd.StarLevel; k++) 
+				{
+					smd.MonsterID.Add(k);
+				}
+				
+				mapData[i,j] = smd;
+			}	
+		}
+
+		mapData[characterInitCoorX,characterInitCoorY].MonsterID.Clear();   
+	}
+}
