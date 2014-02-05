69ec277339b3ce4997d066f2a23b84fd7dc9c16b
PLY <lingyan@big-bang.com>
2014-02-05 14:51:36 +0800

    no message

---
+++ Assets/Scripts/View/Audio/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager {
+
+	private AudioManager() {}
+
+	private AudioManager instance;
+	public AudioManager Instance {
+		get {
+			if( instance == null )
+				instance = new AudioManager();
+			return instance;
+		}
+	}
+
+	public void BgMusicOn() {
+
+	}
+}
