be38e25933268533f92449f53e5431112b069201
leiliang <303040565@qq.com>
2014-02-18 20:30:18 +0800

    no message

--- Assets/Scripts/Utility/GameEnum.cs
+++ Assets/Scripts/Utility/GameEnum.cs
@@ -130,7 +130,7 @@ public enum CommandEnum
 	TrapTargetPoint				= 3044,
 
 	OpenDoor					= 3045,
-
+	RotateDown					= 3046,
 	//Add By Ling yan	
 	LevelUpPanelFocus			= 4000,
 	TransmitStageInfo			= 4001,
