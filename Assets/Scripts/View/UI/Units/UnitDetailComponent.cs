c58a026b747abbdff6eae088488f6e5f0b7856d9
PLY <lingyan@big-bang.com>
2014-02-26 15:44:38 +0800

    no message

--- Assets/Scripts/View/UI/Units/UnitDetailComponent.cs
+++ Assets/Scripts/View/UI/Units/UnitDetailComponent.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using bbproto;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class UnitDetailComponent : ConcreteComponent {
+public class UnitDetailComponent : ConcreteComponent,IUICallback {
+
+	//int maxExp, curLevel, curExp, gotExp, expRiseStep;
 
 	public UnitDetailComponent(string uiName):base(uiName) {}
 	
@@ -20,5 +24,42 @@ public class UnitDetailComponent : ConcreteComponent {
 	public override void DestoryUI () {
 		base.DestoryUI ();
 	}
+
+	public void Callback(object data) {
+
+
+	}
+
+	void GetSkill(object msg) {
+		UnitInfo unitInfo = msg as UnitInfo;
+
+		int skillID;
+		skillID = unitInfo.skill1;
+		GetNormalSkill1(skillID);
+
+	}
+
+	void GetNormalSkill1(int id){
+		Debug.Log("UnitDetailComponent.GetNormalSkill1()");
+
+	}
+
+	void GetNormalSkill2() {
+
+	}
+
+	void GetLeaderSkill() {
+
+	}
+
+	void GetActiveSkill() {
+
+	}
+
+
+
+
+
+
 
 }
