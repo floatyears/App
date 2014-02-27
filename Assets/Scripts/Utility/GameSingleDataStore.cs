d12277f3150b65d8b3204de61f5fe6cb4d2c9cea
leiliang <303040565@qq.com>
2014-02-27 15:21:28 +0800

    no message

--- Assets/Scripts/Utility/GameSingleDataStore.cs
+++ Assets/Scripts/Utility/GameSingleDataStore.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class GameSingleDataStore {
 	private static GameSingleDataStore instance;
 	public static GameSingleDataStore Instance {
-
+		get {
+			if(instance == null) {
+				instance = new GameSingleDataStore();
+			}
+		}
 	}
 
 }
