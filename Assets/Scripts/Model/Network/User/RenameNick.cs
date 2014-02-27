a4a1a7449d807b014a81d90d18f695ce1ee18f1f
kory <119238@qq.com>
2014-02-27 19:13:06 +0800

    no message

---
+++ Assets/Scripts/Model/Network/User/RenameNick.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using bbproto;
+
+public class RenameNick: ProtoManager {
+	private bbproto.ReqRenameNick reqRenameNick;
+	private bbproto.RspRenameNick rspRenameNick;
+
+	public RenameNick(){
+		MsgCenter.Instance.AddListener (CommandEnum.ReqRenameNick, OnReceiveCommand);
+	}
+
+	~RenameNick() {
+		MsgCenter.Instance.RemoveListener (CommandEnum.ReqRenameNick, OnReceiveCommand);
+	}
+
+	public override bool MakePacket () {
+		LogHelper.Log ("RenameNick.MakePacket()...");
+
+		Proto = "auth_user";
+		reqType = typeof(ReqRenameNick);
+		rspType = typeof(RspRenameNick);
+
+		reqRenameNick = new ReqRenameNick ();
+		reqRenameNick.header = new ProtoHeader ();
+		reqRenameNick.header.apiVer = "1.0";
+		reqRenameNick.newNickName = "";
+		
+
+		ErrorMsg err = SerializeData (reqRenameNick); // save to Data for send out
+		
+		return err.Code == ErrorCode.Succeed;
+	}
+
+	public override void OnResponse (bool success) {
+		if (!success) { return; }
+
+		rspRenameNick = InstanceObj as bbproto.RspRenameNick;
+//		LogHelper.Log("authUser response userId:"+rspRenameNick.header.userId);
+
+		//send response to caller
+		MsgCenter.Instance.Invoke (CommandEnum.RspRenameNick, null);
+	}
+
+	void OnReceiveCommand(object data) {
+		LogHelper.Log ("OnReceiveCommand authUser...");
+		Send (); //send request to server
+	}
+
+}
+
---
+++ Server/bbsvr/test/testRename.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"code.google.com/p/goprotobuf/proto"
+	//"fmt"
+	_ "html"
+	//"io"
+	//"io/ioutil"
+	"log"
+	//"math/rand"
+	//"net/http"
+	//"time"
+)
+import (
+	"../src/bbproto"
+	//"../src/common"
+	//"../src/const"
+	//"../src/data"
+	_ "../src/quest"
+	//"../src/user/usermanage"
+	//redis "github.com/garyburd/redigo/redis"
+)
+
+func Rename(uid uint32, newName string) error {
+	msg := &bbproto.ReqRenameNick{}
+	msg.Header = &bbproto.ProtoHeader{}
+	msg.Header.ApiVer = proto.String("1.0.0")
+	msg.Header.SessionId = proto.String("S10298090290")
+	msg.Header.UserId = proto.Uint32(uid)
+
+	msg.NewNickName = proto.String(newName)
+
+	buffer, err := proto.Marshal(msg)
+	if err != nil {
+		log.Printf("Marshal ret err:%v buffer:%v", err, buffer)
+	}
+
+	rspbuff, err := SendHttpPost(bytes.NewReader(buffer), _PROTO_RENAME_NICK)
+	if err != nil {
+		log.Printf("SendHttpPost ret err:%v", err)
+		return err
+	}
+
+	//decode rsp msg
+	log.Printf("-----------------------Response----------------------")
+	rspmsg := &bbproto.RspRenameNick{}
+	if err = proto.Unmarshal(rspbuff, rspmsg); err != nil {
+		log.Printf("ERROR: rsp Unmarshal ret err:%v", err)
+	}
+
+	//print rsp msg
+	log.Printf("Reponse : [%v] error: %v", *rspmsg.Header.Code, *rspmsg.Header.Error)
+
+	return err
+}
+
+func rmain() {
+	Init()
+	Rename(101, "king")
+
+	log.Fatal("bbsvr test client finish.")
+}
