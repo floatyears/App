c978088010e4d94822540a6f9a7d64cb96941049
PLY <lingyan@big-bang.com>
2014-03-04 22:48:09 +0800

    update atlas

--- Assets/PartyPagePanel.cs
+++ Assets/PartyPagePanel.cs
@@ -4,12 +4,17 @@ using System.Collections.Generic;
 
 public class PartyPagePanel : UIComponentUnity {
 
+	int pageIndexOrigin = 1;
+	int currentPartyIndex;
+	int partyTotalCount;
 	UILabel curPartyIndexLabel;
 	UILabel partyCountLabel;
 	UILabel curPartyPrefixLabel;
 	UILabel curPartysuffixLabel;
 	UIButton leftButton;
 	UIButton rightButton;
+	Dictionary< int, string > partyIndexDic = new Dictionary< int, string >();
+	Dictionary< int, UITexture > unitTexureDic = new Dictionary< int, UITexture>();
 
 	public override void Init(UIInsConfig config, IUICallback origin){
 		base.Init(config, origin);
@@ -30,19 +35,37 @@ public class PartyPagePanel : UIComponentUnity {
 	void FindUIElement(){
 		Debug.Log("PartyPagePanel.FindUIElement() : Start");
 
-		curPartyIndexLabel = FindChild<UILabel>("Label_Cur_Party");
-		partyCountLabel = FindChild<UILabel>("Label_Party_Count");
-		curPartyPrefixLabel = FindChild<UILabel>("Label_Party_Index_Prefix");
-		curPartysuffixLabel = FindChild<UILabel>("Label_Party_Index_Suffix");
-		leftButton = FindChild<UIButton>("Button_Left");
-		rightButton = FindChild<UIButton>("Button_Right");
+		FindLabel();
+		FindButton();
+		FindAvatarTexture();
 
 		Debug.Log("PartyPagePanel.FindUIElement() : End");
 	}
 
+	void FindLabel(){
+		curPartyIndexLabel = FindChild<UILabel>("Label_Cur_Party");
+		partyCountLabel = FindChild<UILabel>("Label_Party_Count");
+		curPartyPrefixLabel = FindChild<UILabel>("Label_Party_Index_Prefix");
+		curPartysuffixLabel = FindChild<UILabel>("Label_Party_Index_Suffix");
+	}
+
+	void FindButton(){
+		leftButton = FindChild<UIButton>("Button_Left");
+		rightButton = FindChild<UIButton>("Button_Right");
+	}
+
+	void FindAvatarTexture() {
+		UITexture temp;
+		for( int i = 1; i < 5; i++) {
+			temp = FindChild< UITexture >("PartyPages/Unit" + i.ToString() + "/role" );
+			temp.enabled = false;
+			unitTexureDic.Add(i, temp);
+		}
+	}
+
 	void SetUIElement(){
 		Debug.Log("PartyPagePanel.SetUIElement() : Start");
-
+		SetIndexTextDic();
 		UIEventListener.Get(leftButton.gameObject).onClick = PageBack;
 		UIEventListener.Get(rightButton.gameObject).onClick = PageForward;
 
@@ -50,8 +73,24 @@ public class PartyPagePanel : UIComponentUnity {
 		Debug.Log("PartyPagePanel.SetUIElement() : End");
 	}
 
+	void SetIndexTextDic() {
+		partyIndexDic.Add( 1, "st");
+		partyIndexDic.Add( 2, "nd");
+		partyIndexDic.Add( 3, "rd");
+		partyIndexDic.Add( 4, "th");
+		partyIndexDic.Add( 5, "th");
+	}
+
 	void PageBack(GameObject button){
 		Debug.Log("PartyPagePanel.PageBack() : Start");
+
+		currentPartyIndex = Mathf.Abs( (currentPartyIndex - 1) % partyTotalCount );
+		if( currentPartyIndex == 0 )
+			currentPartyIndex = partyTotalCount ;
+		curPartyIndexLabel.text = currentPartyIndex.ToString();
+		curPartyPrefixLabel.text = currentPartyIndex.ToString();
+		curPartysuffixLabel.text = partyIndexDic[ currentPartyIndex ].ToString();
+
 		//call logic 
 		string callerName = "PageBack";
 		ExcuteCallback(callerName);
@@ -63,8 +102,19 @@ public class PartyPagePanel : UIComponentUnity {
 	void PageForward(GameObject go){
 		Debug.Log("PartyPagePanel.PageForward() : Start");
 
+		currentPartyIndex++;
+		if (currentPartyIndex > partyTotalCount) {
+			currentPartyIndex = pageIndexOrigin;
+		} 
+		curPartyPrefixLabel.text = currentPartyIndex.ToString();
+		curPartyPrefixLabel.text = currentPartyIndex.ToString();
+		curPartysuffixLabel.text = partyIndexDic[ currentPartyIndex ].ToString();
+
+
 		Debug.Log("PartyPagePanel.PageForward() : End");
 	}
+
+
 
 	void ResetUIElement(){
 //		ExcuteCallback();
