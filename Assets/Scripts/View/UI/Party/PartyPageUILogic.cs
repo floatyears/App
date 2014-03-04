740e9f628e1b987ab8df6974d9fb715756935e4e
PLY <lingyan@big-bang.com>
2014-03-04 18:03:50 +0800

    no message

---
+++ Assets/Scripts/View/UI/Party/PartyPageUILogic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartyPageUILogic : ConcreteComponent {
+
+	public PartyPageUILogic(string uiName):base(uiName) {}
+
+	public override void Callback(object data){
+		base.Callback(data);
+//		ExcuteCallback()
+	}
+
+}
