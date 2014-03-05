f61ef26e5a8dafddeeae1de092f0d3366550800f
PLY <lingyan@big-bang.com>
2014-03-05 16:31:33 +0800

    before refactor

---
+++ Assets/Scripts/View/UI/Party/MyUnitDragUILogic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PartyDragUILogic : ConcreteComponent {
+
+	public PartyDragUILogic(string uiName):base(uiName) {}
+
+	public override void ShowUI(){
+		base.ShowUI();
+
+	}
+
+	void GetPartyData(){
+
+	}
+}
---
+++ Assets/SelectUnitInfoWindow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitInfoBox : UIComponentUnity {
+
+	GameObject buttonChoose;
+	GameObject buttonViewInfo;
+	GameObject buttonExit;
+
+	public override void Init(UIInsConfig config, IUICallback origin){
+		base.Init(config, origin);
+		InitUIElement();
+	}
+	public override void ShowUI(){
+		base.ShowUI();
+	}
+
+	public override void HideUI(){
+		base.HideUI();
+	}
+
+	void InitUIElement(){
+		InitButton();
+	}
+
+	void InitButton(){
+		buttonChoose = transform.FindChild("btn_choose").gameObject;
+		UIEventListener.Get(buttonChoose).onClick = Choose;
+		buttonViewInfo = transform.FindChild("btn_see_info").gameObject;
+		UIEventListener.Get(buttonViewInfo).onClick = ViewInfo;
+		buttonExit = transform.FindChild("btn_see_info").gameObject;
+		UIEventListener.Get(buttonExit).onClick = Exit;
+	}
+
+	void Choose(GameObject btn){
+	}
+
+	void ViewInfo(GameObject btn){
+	}
+
+	void Exit(GameObject btn){
+	}
+
+	void ShowInfo(object info){
+	}
+
+
+}
