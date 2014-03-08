965c16f824a4f1f9c10965c98c57697d2040f3af
Ailenswpu <renkaiswpu@gmail.com>
2014-03-08 10:57:58 +0800

    add getway

--- config_server/app/controllers/unit_infos_controller.rb
+++ config_server/app/controllers/unit_infos_controller.rb
@@ -14,7 +14,8 @@ class UnitInfosController < ApplicationController
 
   # GET /unit_infos/new
   def new
-    @units = {"请选择卡牌信息" => "请选择卡牌信息" }.merge $redis.keys.inject({}){|hsh,key| hsh[key] = key.split("_")[2].to_i if key.include?("X_UNIT_");hsh}
+    unit_keys =  $redis.keys.map{|k|k if k.include?("X_UNIT_")}.compact
+    @units = {"请选择卡牌信息" => "请选择卡牌信息" }.merge unit_keys.inject({}){|hsh,key| hsh[key] = key.split("_")[2].to_i;hsh}
   end
 
   # GET /unit_infos/1/edit
