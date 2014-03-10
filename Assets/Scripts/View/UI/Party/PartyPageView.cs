f6c73d9628843341060682e003c2067644001c94
PLY <lingyan@big-bang.com>
2014-03-10 15:29:40 +0800

    no message

---
+++ Assets/Scripts/View/UI/Party/PartyPageView.cs
@@ -0,0 +1,256 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PartyPageView : UIComponentUnity {
+	int currentPos = -1;
+	UILabel curPartyIndexLabel;
+	UILabel partyCountLabel;
+	UILabel curPartyPrefixLabel;
+	UILabel curPartysuffixLabel;
+	UIButton leftButton;
+	UIButton rightButton;
+	Dictionary< int, string > partyIndexDic = new Dictionary< int, string >();
+	Dictionary<GameObject, int> itemDic = new Dictionary<GameObject, int>();
+
+	List<UITexture> texureList = new List<UITexture>();
+	bool InitSymbol = false;
+	public override void Init(UIInsConfig config, IUICallback origin){
+
+		base.Init(config, origin);
+		FindUIElement();
+		InitUIElement();
+	}
+
+	public override void ShowUI(){
+		base.ShowUI();
+		currentPos = -1;
+		SetUIElement();
+		ShowTween();
+	}
+
+	public override void HideUI(){
+     
+		base.HideUI();
+		ResetUIElement();
+	}
+	
+	void FindUIElement(){
+		Debug.Log("PartyPagePanel.FindUIElement() : Start...");
+
+		FindLabel();
+		FindButton();
+		FindTexture();
+
+		Debug.Log("PartyPagePanel.FindUIElement() : End...");
+	}
+
+	void FindLabel(){
+		curPartyIndexLabel = FindChild<UILabel>("Label_Cur_Party");
+		partyCountLabel = FindChild<UILabel>("Label_Party_Count");
+		curPartyPrefixLabel = FindChild<UILabel>("Label_Party_Index_Prefix");
+		curPartysuffixLabel = FindChild<UILabel>("Label_Party_Index_Suffix");
+	}
+
+	void FindButton(){
+		leftButton = FindChild<UIButton>("Button_Left");
+		rightButton = FindChild<UIButton>("Button_Right");
+	}
+
+	void FindTexture() {
+		UITexture tex;
+		GameObject go;
+		for( int i = 1; i < 5; i++) {
+			tex = FindChild< UITexture >("Unit" + i.ToString() + "/role" );
+			texureList.Add(tex);
+			go = transform.FindChild("Unit" + i.ToString() ).gameObject;
+			UIEventListener.Get(go).onClick = ClickItem;
+			itemDic.Add( go, i );
+		}
+	}
+
+	void InitUIElement(){
+		InitIndexTextDic();
+		partyCountLabel.text = UIConfig.PartyMaxCount.ToString();
+	}
+
+	void SetUIElement(){
+		Debug.Log("PartyPagePanel.SetUIElement() : Start...");
+		UIEventListener.Get(leftButton.gameObject).onClick = PagePrev;
+		UIEventListener.Get(rightButton.gameObject).onClick = PageNext;
+		Debug.Log("PartyPagePanel.SetUIElement() : End...");
+	}
+
+	void InitIndexTextDic() {
+		partyIndexDic.Add( 1, "st");
+		partyIndexDic.Add( 2, "nd");
+		partyIndexDic.Add( 3, "rd");
+		partyIndexDic.Add( 4, "th");
+		partyIndexDic.Add( 5, "th");
+	}
+
+	void ClickItem(GameObject go){
+
+		if( !itemDic.ContainsKey(go) ){
+			Debug.Log("PartyPagePanel.ClickItem(), itemDic NOT ContainsKey : " + go.name);
+			return;
+		}
+
+		string callName = "PickItem";
+		int pos = itemDic[ go ];
+		CallBackDispatcherArgs cbd = new CallBackDispatcherArgs( callName, pos );
+		LogHelper.Log("PartyPagePanel.ClickItem(), click the item" + itemDic[ go ].ToString() + ", wait respone...");
+		ExcuteCallback( cbd );
+
+	}
+
+
+	void PagePrev(GameObject button){
+		Debug.Log("PartyPagePanel.PagePrev() : Start");
+
+		CallBackDispatcherArgs cbd = new CallBackDispatcherArgs( "TurnPage", "prev" );
+		LogHelper.Log("PartyPagePanel.ClickItem(), click the BackArrow, wait respone...");
+
+		ExcuteCallback( cbd );
+
+		Debug.Log("PartyPagePanel.ExcuteCallback() : End");
+	} 
+
+	void PageNext(GameObject go){
+		Debug.Log("PartyPagePanel.PageNext() : Start");
+
+		CallBackDispatcherArgs cbd = new CallBackDispatcherArgs( "TurnPage", "next" );
+		LogHelper.Log("PartyPagePanel.ClickItem(), click the BackArrow, wait respone...");
+		ExcuteCallback( cbd );
+
+		Debug.Log("PartyPagePanel.PageNext() : End");
+	}
+	
+	void ResetUIElement(){
+		Debug.Log("PartyPagePanel.ResetUIElement() : Start");
+		Debug.Log("PartyPagePanel.ResetUIElement() : End");
+	}
+	
+	void ChangeTexure(int pos,Texture2D tex){
+		if(tex == null ){
+			return;
+		}
+	
+		foreach (var item in itemDic) {
+			if( pos == item.Value ){
+				texureList[ pos-1 ].mainTexture = tex;
+			} 
+		}
+	}
+	
+	void LoseFocus(object args){
+		Debug.Log("PartyPagePanel.LoseFocus() : Start...");
+
+		foreach (var item in itemDic) {
+			item.Key.transform.FindChild("High_Light").gameObject.SetActive(false);
+		}
+
+		Debug.Log("PartyPagePanel.LoseFocus() : End...");
+	}  
+
+	//Light the click Item
+	void SetHighLight(object args){
+		int pos = (int)args;
+		Debug.Log("PartyPagePanel.SetHighLight() : Sprite Pos is : " + pos);
+		foreach (var item in itemDic) {
+			if( pos == item.Value ){
+				currentPos = pos;
+				item.Key.transform.FindChild("High_Light").gameObject.SetActive(true);
+			} else {
+				item.Key.transform.FindChild("High_Light").gameObject.SetActive(false);
+			}
+		}
+
+		Debug.Log("PartyPagePanel.SetHighLight() : End...");
+	}
+
+	void ShowTween(){
+		TweenPosition[ ] list = 
+			gameObject.GetComponentsInChildren< TweenPosition >();
+		if (list == null)
+			return;
+		foreach (var tweenPos in list){		
+			if (tweenPos == null)
+				continue;
+			tweenPos.Reset();
+			tweenPos.PlayForward();
+		}
+	}
+
+	void RefreshIndexLabel(object args){
+		int index = ( int )args;
+		Debug.Log("PartyPagePanel.RefreshIndexLabel(), index is " + index);	
+		
+		curPartyPrefixLabel.text = index.ToString();
+		curPartysuffixLabel.text = partyIndexDic[ index ].ToString();
+		curPartyIndexLabel.text = index.ToString();
+	}
+	
+	void RefreshItemTexture(object args){
+		List<Texture2D> tex2dList = args as List<Texture2D>;
+		Debug.Log("PartyPagePanel.UpdateTexture(), Start...");
+		for (int i = 0; i < tex2dList.Count; i++) {
+			if(tex2dList[ i ] == null){
+				Debug.LogError(string.Format("PartyPagePanel.UpdateTexture(), Pos[{0}] source is null, do nothing!", i));
+				continue;
+			} 
+			else {
+				texureList[ i ].mainTexture = tex2dList[ i ];
+				Debug.Log(string.Format("PartyPagePanel.UpdateTexture(), Pos[{0}] texture is showing", i));
+			}
+		}
+		Debug.Log("PartyPagePanel.UpdateTexture(), End...");
+	}
+
+	public override void Callback(object data){
+		base.Callback(data);
+		CallBackDispatcherArgs cbdArgs = data as CallBackDispatcherArgs;
+		
+		switch ( cbdArgs.funcName ){
+			case "RefreshPartyIndexView" : 
+				CallBackDispatcherHelper.DispatchCallBack(RefreshIndexLabel, cbdArgs);
+				break;
+			case "RefreshPartyItemView" : 
+				CallBackDispatcherHelper.DispatchCallBack(RefreshItemTexture, cbdArgs);
+                        break;
+               	 	case "LightCurSprite" :
+				CallBackDispatcherHelper.DispatchCallBack(SetHighLight, cbdArgs);
+                        	break;
+                	case "DrakAllSprite" :
+				CallBackDispatcherHelper.DispatchCallBack(LoseFocus, cbdArgs);
+                        	break;
+			case "ReplaceItemView" :
+				CallBackDispatcherHelper.DispatchCallBack(ReplaceItemView, cbdArgs);
+				break;
+			case "ClearItem" :
+				CallBackDispatcherHelper.DispatchCallBack(ClearItemView, cbdArgs);
+				break;
+                default:
+                        break;
+                }
+                
+        }
+
+	void ClearItemView(object args){
+		int position = (int)args;
+		Debug.Log("PartyPagePanel.ClearItemView(), receive the call, to clear the view of item " + position);
+	}
+
+	void ReplaceItemView(object args){
+		Dictionary<string,object> argsDic = args as Dictionary<string, object>;
+
+		int position = (int)argsDic["position"] ;
+		TUserUnit tuu = argsDic["unit"] as TUserUnit;
+		Texture2D texture = tuu.UnitInfo.GetAsset( UnitAssetType.Avatar );
+	
+		ChangeTexure(position, texture);
+
+		Debug.Log("PartyPagePanel.ReplaceItemView(), End...");
+	}
+	
+}
---
+++ Assets/Scripts/View/UI/Party/PartyUnitsView.cs
@@ -0,0 +1,220 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class PartyUnitsView : UIComponentUnity {
+	DragPanel dragPanel;
+	bool exchange = false;
+        List<TUserUnit> userUnitInfoList = new List<TUserUnit>();
+	Dictionary<string, object> dragPanelArgs = new Dictionary<string, object>();
+	Dictionary<GameObject, TUserUnit> myUnitInfoDic = new Dictionary<GameObject, TUserUnit>();
+	List<UnitInfoStruct> unitInfoStruct = new List<UnitInfoStruct>();
+
+	public override void Init(UIInsConfig config, IUICallback origin){
+		base.Init(config, origin);
+		InitDragPanel();
+	}
+
+	public override void ShowUI(){
+		base.ShowUI();
+
+		InvokeCrossShow(0.1f, 1.0f);
+		OnLightDragItem(true);
+		ShowTween();
+	}
+
+	public override void HideUI(){
+		base.HideUI();
+	}
+
+	void InitDragPanel(){
+		if ( GlobalData.myUnitList != null)
+			userUnitInfoList.AddRange(GlobalData.myUnitList.GetAll().Values);
+		else {
+			Debug.Log("GlobalData.myUnitList is null, return");
+			return;
+		}
+
+		if(userUnitInfoList == null ){
+			Debug.LogWarning("userUnitInfoList is null ");
+			return;
+		}
+
+		int unitCount = userUnitInfoList.Count;
+		LogHelper.Log("My Unit Count : " + unitCount);
+		string itemSourcePath = "Prefabs/UI/Friend/UnitItem";
+		GameObject unitItem =  Resources.Load( itemSourcePath ) as GameObject;
+		GameObject rejectItem = Resources.Load("Prefabs/UI/Friend/RejectItem") as GameObject ;
+
+		InitDragPanelArgs();
+
+		dragPanel =new DragPanel("UnitScroller", unitItem);
+		dragPanel.CreatUI();
+		dragPanel.AddItem(1, rejectItem);
+		dragPanel.AddItem(unitCount, unitItem);
+		FillDragPanel( dragPanel );
+
+		dragPanel.RootObject.SetScrollView(dragPanelArgs);
+	}
+
+	protected DragPanel CreateDragPanel( string name, int count, GameObject item){
+		DragPanel panel = new DragPanel(name,item);
+		panel.CreatUI();
+		panel.AddItem( count, item);
+		return panel;
+	}
+
+	void FillDragPanel(DragPanel panel){
+		if( panel == null ){
+			Debug.LogError( "PartyUnitsView.FillDragPanel(), DragPanel is null, return!");
+			return;
+		}
+	
+		for( int i = 0; i < panel.ScrollItem.Count; i++){
+
+			GameObject scrollItem = panel.ScrollItem[ i ];
+
+			if( i == 0 ){
+				UIEventListenerCustom.Get( scrollItem ).onClick = ClickRejectItem;
+				continue;
+			}
+
+			TUserUnit uuItem = userUnitInfoList[ i - 1 ] ;
+			myUnitInfoDic.Add( scrollItem, uuItem );
+			
+			StoreLabelInfo( scrollItem);
+			ShowItem( scrollItem );
+			AddEventListener( scrollItem );
+		}
+	}
+
+	void ShowItem( GameObject item){
+		ShowMask(item,true);
+		GameObject avatarGo = item.transform.FindChild( "Texture_Avatar").gameObject;
+		UITexture avatarTex = avatarGo.GetComponent< UITexture >();
+		
+		uint uid = myUnitInfoDic[item].UnitID;
+		avatarTex.mainTexture = GlobalData.unitInfo[ uid ].GetAsset(UnitAssetType.Avatar);
+		
+		int addAttack = myUnitInfoDic[ item ].AddAttack;
+
+		int addHp = myUnitInfoDic[ item ].AddHP;
+
+		int level = myUnitInfoDic[ item ].Level;
+
+		int addPoint = addAttack + addHp;
+	}
+
+	void ClickItem(GameObject item){
+		AudioManager.Instance.PlayAudio(AudioEnum.sound_click);
+		CallBackDispatcherArgs cbd = new CallBackDispatcherArgs("ItemClick", myUnitInfoDic[ item ]);
+		LogHelper.Log("PartyUnitsView.ClickDragItem(), click drag item, call view respone...");
+		ExcuteCallback( cbd );
+	}
+
+	void PressItem(GameObject item ){
+		TUserUnit unitInfo = myUnitInfoDic[ item ];
+		UIManager.Instance.ChangeScene(SceneEnum.UnitDetail );
+		MsgCenter.Instance.Invoke(CommandEnum.ShowUnitDetail, unitInfo);	
+	}
+
+	void ShowMask( GameObject target, bool canMask) {
+		GameObject maskSpr = target.transform.FindChild("Mask").gameObject;
+		maskSpr.gameObject.SetActive( canMask );
+	}
+
+	void AddEventListener( GameObject item){
+		UIEventListenerCustom.Get( item ).onClick = ClickItem;
+		UIEventListenerCustom.Get( item ).LongPress = PressItem;
+	}
+
+	void StoreLabelInfo(GameObject item){
+		TUserUnit tuu = myUnitInfoDic[ item ];
+		UnitInfoStruct infoStruct = new UnitInfoStruct();
+		infoStruct.text1 = tuu.Level.ToString();
+		infoStruct.text2 = (tuu.AddHP + tuu.AddAttack).ToString();
+		infoStruct.targetLabel = item.transform.FindChild("Label_Info").GetComponent<UILabel>();
+		unitInfoStruct.Add(infoStruct);
+	}
+
+	void InitDragPanelArgs(){
+		dragPanelArgs.Add("parentTrans",	transform);
+		dragPanelArgs.Add("scrollerScale",	Vector3.one);
+		dragPanelArgs.Add("scrollerLocalPos",	-28 * Vector3.up);
+		dragPanelArgs.Add("position", 		Vector3.zero);
+		dragPanelArgs.Add("clipRange", 		new Vector4(0, -120, 640, 400));
+		dragPanelArgs.Add("gridArrange", 	UIGrid.Arrangement.Vertical);
+		dragPanelArgs.Add("maxPerLine",		3);
+		dragPanelArgs.Add("scrollBarPosition",	new Vector3(-320, -315, 0));
+		dragPanelArgs.Add("cellWidth", 		110);
+		dragPanelArgs.Add("cellHeight",		110);
+	}
+
+	void CrossShow(){
+		if(exchange){
+			for (int i = 0 ; i< unitInfoStruct.Count; i++) {
+				unitInfoStruct[ i ].targetLabel.text = string.Format( "+{0}", unitInfoStruct[ i ].text2);
+				unitInfoStruct[ i ].targetLabel.color = Color.yellow;
+			}
+			exchange = false;
+		}
+		else{
+			for (int i = 0 ; i< unitInfoStruct.Count; i++) {
+				unitInfoStruct[ i ].targetLabel.text = string.Format( "Lv{0}", unitInfoStruct[ i ].text1);
+				unitInfoStruct[ i ].targetLabel.color = Color.red;                          
+			}
+                        exchange = true;
+                }
+        }
+
+	void ShowTween(){
+		TweenPosition[ ] list = gameObject.GetComponentsInChildren< TweenPosition >();
+		if (list == null)
+			return;
+		foreach (var tweenPos in list){		
+			if (tweenPos == null)
+				continue;
+			tweenPos.Reset();
+			tweenPos.PlayForward();
+		}
+	}
+
+	void InvokeCrossShow(float start, float cycle){
+		if(IsInvoking("CrossShow")) {
+			CancelInvoke("CrossShow");
+		}
+                InvokeRepeating("CrossShow", start, cycle);
+        }
+
+        void OnLightDragItem(bool b){
+		LogHelper.Log("PartyUnitsView.ActivateAllMask(), Receive callBack from Logic, to activate panel...");
+		foreach (var item in myUnitInfoDic)
+			ShowMask(item.Key, b);
+
+		LogHelper.Log("PartyUnitsView.ActivateAllMask(), End...");
+	}
+
+	void ClickRejectItem(GameObject go){
+		Debug.Log("PartyUnitsView.ClickRejectItem(), Receive reject item click, request logic...");
+		CallBackDispatcherArgs cbd = new CallBackDispatcherArgs("ClickReject", null);
+		ExcuteCallback(cbd);
+	}
+
+	
+	public override void Callback(object data){
+		base.Callback(data);
+		CallBackDispatcherArgs cbd = data as CallBackDispatcherArgs;
+		switch (cbd.funcName){
+			case "activate" : 
+                        OnLightDragItem(false);
+                        break; 
+                default:
+                        break;
+                }	
+        }
+
+}
+
+
+
+
+
