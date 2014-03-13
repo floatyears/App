7b8e3fd88fe9e9605033ae8085c3facdd552dfdd
kory <119238@qq.com>
2014-03-13 13:15:59 +0800

    modify evolve done

--- Server/bbsvr/src/user/api_auth_user.go
+++ Server/bbsvr/src/user/api_auth_user.go
@@ -17,9 +17,9 @@ import (
 	"common/Error"
 	"common/log"
 	"data"
-	"event"
 	"model/friend"
 	"model/user"
+	"model/unit"
 )
 
 /////////////////////////////////////////////////////////////////////////////
@@ -204,7 +204,7 @@ func (t AuthUser) ProcessLogic(reqMsg *bbproto.ReqAuthUser, rspMsg *bbproto.RspA
 
 	}
 
-	rspMsg.EvolveType = event.GetEvolveType()
+	rspMsg.EvolveType = unit.GetTodayEvolveType()
 
 	log.T(">>>>>>>>>>>>AuthUser RspMsg<<<<<<<<<<<<<<")
 	log.T("\tUserinfo: %+v", rspMsg.User)
