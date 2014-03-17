28d9d0de66ea96c572692c3124f13b10e5a7b03a
leiliang <303040565@qq.com>
2014-03-17 21:07:36 +0800

    no message

---
+++ Assets/Scripts/Model/TDataClass/TCityInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using bbproto;
+
+public class TCityInfo : ProtobufDataBase {
+	private CityInfo instance;
+
+	public TCityInfo (CityInfo ci) : base (ci) {
+		instance = ci;
+	}
+	public CityInfo cityInfo {
+		get { return instance; }
+	}
+
+
+}
---
+++ Assets/Scripts/Model/TDataClass/TQuestInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using bbproto;
+
+public class TQuestInfo : ProtobufDataBase {
+	private QuestInfo instance;
+	public TQuestInfo (QuestInfo qi) : base (qi) {
+		instance = qi;
+	}
+
+	public QuestInfo questInfo {
+		get {return instance;}
+	}
+
+
+}
