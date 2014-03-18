da72b46a9259f7db84a1165c82fec08f32987b94
PLY <lingyan@big-bang.com>
2014-03-18 09:45:49 +0800

    reset to current version

--- Assets/Scripts/View/UI/FriendList/FriendListLogic.cs
+++ Assets/Scripts/View/UI/FriendList/FriendListLogic.cs
@@ -58,41 +58,54 @@ public class FriendListLogic : ConcreteComponent
 
 	void NoteRefuseAll(object args)
 	{
-		MsgCenter.Instance.Invoke(CommandEnum.NoteRefuseAll, null);
+		MsgCenter.Instance.Invoke(CommandEnum.NoteInformation, ConfigNoteMessage.refuseAllFriendIn);
 	}
 
-	void EnsureRefuseAllReception(object msg)
+	void RefuseAllApplyFromOthers(object msg)
 	{
-		Debug.LogError("66666666666666666666");
 		RefuseFriendAll();
 	}
 
 	void AddCommandListener()
 	{
-		MsgCenter.Instance.AddListener(CommandEnum.EnsureUpdateFriend, GetNewestFriendList);
-		MsgCenter.Instance.AddListener(CommandEnum.EnsureDeleteFriend, DeleteFriendCurrentPicked);          
-		MsgCenter.Instance.AddListener(CommandEnum.EnsureDeleteApply, EnsureDeleteApply);
-		MsgCenter.Instance.AddListener(CommandEnum.EnsureRefuseAll, EnsureRefuseAllReception);
-	}
+		MsgCenter.Instance.AddListener(CommandEnum.EnsureUpdateFriend, UpdateFriendList);
+		MsgCenter.Instance.AddListener(CommandEnum.EnsureDeleteFriend, DeleteFriendPicked);          
+		MsgCenter.Instance.AddListener(CommandEnum.EnsureDeleteApply, DeleteMyApply);
+		MsgCenter.Instance.AddListener(CommandEnum.EnsureRefuseAll, RefuseAllApplyFromOthers);
+		MsgCenter.Instance.AddListener(CommandEnum.EnsureAcceptApply, AcceptApplyFromOther);
+		MsgCenter.Instance.AddListener(CommandEnum.EnsureRefuseSingleApply, DeleteApplyFromOther);     
+                
+        }
 
-	void EnsureDeleteApply(object msg)
+	void DeleteMyApply(object msg)
 	{
 		CancelFriendRequest(currentFriendPicked.UserId);
 	}
 		
 	void RemoveCommandListener()
 	{
-		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureUpdateFriend, GetNewestFriendList);
-		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureDeleteFriend, DeleteFriendCurrentPicked);
-		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureDeleteApply, EnsureDeleteApply);
-		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureRefuseAll, EnsureRefuseAllReception);
-
-
-                
+		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureUpdateFriend, UpdateFriendList);
+		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureDeleteFriend, DeleteFriendPicked);
+		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureDeleteApply, DeleteMyApply);
+		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureRefuseAll, RefuseAllApplyFromOthers);        
+        MsgCenter.Instance.RemoveListener(CommandEnum.EnsureAcceptApply, AcceptApplyFromOther);
+		MsgCenter.Instance.RemoveListener(CommandEnum.EnsureRefuseSingleApply, DeleteApplyFromOther);     
 	}
+
+	void AcceptApplyFromOther(object msg){
+		Debug.LogError("FriendListLogic.AcceptApplyFromOther(), receive the message, to accept apply from other player...");
+//		AcceptFriend.SendRequest(OnAcceptFriend, currentFriendPicked.UserId);
+		AcceptFriendRequest(currentFriendPicked.UserId);
+	}
+
+	void DeleteApplyFromOther(object msg){
+		Debug.LogError("FriendListLogic.DeleteApplyFromOther(), receive the message, to delete apply from other player...");
+		RefuseFriend(currentFriendPicked.UserId);
+	}
+
 	void NoteFriendUpdate(object args)
 	{
-		MsgCenter.Instance.Invoke(CommandEnum.NoteFriendUpdate, null);
+		MsgCenter.Instance.Invoke(CommandEnum.NoteInformation, ConfigNoteMessage.friendUpdateSubmit);
 	}
 	
 	List<TFriendInfo> CurrentFriendListData()
@@ -105,11 +118,11 @@ public class FriendListLogic : ConcreteComponent
 				break;
 			case SceneEnum.Apply: 
 				Debug.Log("CurrentFriendListData() friendIn Count {1}" + DataCenter.Instance.FriendList.FriendIn.Count); 
-				return DataCenter.Instance.FriendList.FriendIn;
+				return DataCenter.Instance.FriendList.FriendOut;
 				break;
 			case SceneEnum.Reception: 
 				Debug.Log("CurrentFriendListData() friendOut Count {2}" + DataCenter.Instance.FriendList.FriendOut.Count); 
-				return DataCenter.Instance.FriendList.FriendOut;
+				return DataCenter.Instance.FriendList.FriendIn;
 				break;
 			
 			default:
@@ -120,13 +133,13 @@ public class FriendListLogic : ConcreteComponent
 
 	}
 
-	void DeleteFriendCurrentPicked(object msg)
+	void DeleteFriendPicked(object msg)
 	{
 		Debug.LogError("FriendListLogic.DeleteFriendCurrentPicked(), Start...");
 		DelFriend.SendRequest(OnDelFriend, currentFriendPicked.UserId);
 	}
 
-	void GetNewestFriendList(object args)
+	void UpdateFriendList(object args)
 	{
 		//ReqSever
 		GetFriendList.SendRequest(OnGetFriendList);
@@ -163,7 +176,7 @@ public class FriendListLogic : ConcreteComponent
 		ShowUI();
 	}
 
-    void RefuseFriend(uint friendUid)
+	void RefuseFriend(uint friendUid)
 	{
 		DelFriend.SendRequest(OnDelFriend, friendUid);
 	}
@@ -192,15 +205,15 @@ public class FriendListLogic : ConcreteComponent
 	void AcceptFriendRequest(uint friendUid){
 		AcceptFriend.SendRequest(OnAcceptFriend, friendUid);
 	}
-    
+
 	void OnAcceptFriend(object data)
 	{
 		if (data == null)
 			return;
         
-		LogHelper.Log("TFriendList.OnRspAddFriend() begin");
+		LogHelper.LogError("TFriendList.OnRspAddFriend() begin");
 		LogHelper.Log(data);
-		bbproto.RspAddFriend rsp = data as bbproto.RspAddFriend;
+		bbproto.RspAcceptFriend rsp = data as bbproto.RspAcceptFriend;
         
 		if (rsp.header.code != (int)ErrorCode.SUCCESS)
 		{
@@ -235,9 +248,14 @@ public class FriendListLogic : ConcreteComponent
 		bbproto.FriendList inst = rsp.friends;
 		LogHelper.LogError("OnRspDelFriend friends {0}", rsp.friends);
         
-
+		DataCenter.Instance.FriendList.RefreshFriendList(inst);
+		if (DataCenter.Instance.FriendList.Friend.Count == 1)
+		{
+			DataCenter.Instance.FriendList.clearFriend();
+		}
+        
 		// test
-		LogHelper.Log("OnAcceptFriend, test first friend. nick name" + CurrentFriendListData() [1].NickName);
+//		LogHelper.Log("OnAcceptFriend, test first friend. nick name" + CurrentFriendListData() [1].NickName);
 		HideUI();
 		ShowUI();
 	}
@@ -254,10 +272,9 @@ public class FriendListLogic : ConcreteComponent
 		}
 		if (UIManager.Instance.baseScene.CurrentScene == SceneEnum.FriendList)
 			MsgCenter.Instance.Invoke(CommandEnum.FriendBriefInfoShow, currentFriendPicked);
-		if (UIManager.Instance.baseScene.CurrentScene == SceneEnum.Apply)
+		if (UIManager.Instance.baseScene.CurrentScene == SceneEnum.Apply 
+		    ||UIManager.Instance.baseScene.CurrentScene == SceneEnum.Reception)
 			MsgCenter.Instance.Invoke(CommandEnum.ViewApplyInfo, currentFriendPicked);
-//		if (UIManager.Instance.baseScene.CurrentScene == SceneEnum.Reception)
-//			MsgCenter.Instance.Invoke(CommandEnum.NoteRefuseAll, currentFriendPicked);
 	}
 
 	void ViewUnitDetailInfo(object args)
@@ -305,7 +322,7 @@ public class FriendListLogic : ConcreteComponent
 		for (int i = 0; i < CurrentFriendListData().Count; i++)
 		{
 //			LogHelper.LogError("Global.friends:i={0}, friends:{1} fUserId:{2}", i, DataCenter.Instance.FriendList[ i ],DataCenter.Instance.FriendList[ i ].UserId);
-			LogHelper.LogError("Global.friends:i={0}, friends.UserUnit:{1}", i, CurrentFriendListData() [i].UserUnit);
+			//LogHelper.LogError("Global.friends:i={0}, friends.UserUnit:{1}", i, CurrentFriendListData() [i].UserUnit);
 			tuuList.Add(CurrentFriendListData() [i].UserUnit);
 		}
 
