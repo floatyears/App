8ef77293d2a26b9934ece8366238255006db41c3
yuechuzhao <oisiiryouri@outlook.com>
2014-03-30 22:33:01 +0800

    no message

--- Assets/Scripts/Utility/DebugHelper.cs
+++ Assets/Scripts/Utility/DebugHelper.cs
@@ -5,7 +5,7 @@ public sealed class DebugHelper{
 	/// <summary>
 	/// The DEBUG. When development overed, set debug = false
 	/// </summary>
-	public static bool DEBUG = true;
+	public static bool DEBUG = false;
 
 	private DebugHelper(){}
 
