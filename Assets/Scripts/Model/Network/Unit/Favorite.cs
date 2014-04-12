4db5a8eee86ae10398e97250c2399e8d9318ca43
kory <119238@qq.com>
2014-04-12 15:45:11 +0800

    unit Favorite

---
+++ Assets/Scripts/Model/Network/Unit/Favorite.cs
@@ -0,0 +1,51 @@
+
+using System;
+using UnityEngine;
+using System.Collections;
+using bbproto;
+
+
+public class UnitFavorite: ProtoManager {
+	// req && rsp
+	private bbproto.ReqUnitFavorite reqUnitFavorite;
+	private bbproto.RspUnitFavorite rspUnitFavorite;
+	// data
+	private uint uniqueId;
+	private EFavoriteAction action;
+	
+	public UnitFavorite() {
+	}
+	
+	~UnitFavorite () {
+	}
+	
+	public static void SendRequest(DataListener callBack, uint uniqueid, EFavoriteAction action) {
+		this.uniqueId = uniqueid;
+		this.action = action;
+		
+		UnitFavorite retireQuest = new UnitFavorite();
+		retireQuest.OnRequest(null, callBack);
+	}
+	
+	public override bool MakePacket() {
+		Proto = Protocol.UNIT_FAVORITE;
+		reqType = typeof(ReqUnitFavorite);
+		rspType = typeof(RspUnitFavorite);
+		
+		reqUnitFavorite = new ReqUnitFavorite();
+		reqUnitFavorite.header = new ProtoHeader();
+		reqUnitFavorite.header.apiVer = Protocol.API_VERSION;
+		reqUnitFavorite.header.userId = DataCenter.Instance.UserInfo.UserId;
+		
+		//request params
+		reqUnitFavorite.unitUniqueId = this.uniqueId;
+		reqUnitFavorite.action = this.action;
+		
+		ErrorMsg err = SerializeData(reqUnitFavorite); // save to Data for send out
+		
+		return (err.Code == (int)ErrorCode.SUCCESS);
+	}
+	
+}
+
+
