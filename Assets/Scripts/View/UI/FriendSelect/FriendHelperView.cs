3450159210c2432301c8b38b00887d9be5745e8e
PLY <lingyan@big-bang.com>
2014-04-26 16:15:39 +0800

    complete new ui

--- Assets/Scripts/View/UI/FriendSelect/FriendHelperView.cs
+++ Assets/Scripts/View/UI/FriendSelect/FriendHelperView.cs
@@ -2,39 +2,30 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class FriendHelperView : UIComponentUnity{
-	private UnitBaseInfo friendBaseInfo;
-	private GameObject itemLeft;
-	private DragPanel dragPanel;
-    private UIImageButton startQuestBtn;
-	private UILabel rightIndexLabel;
+//	private UnitBaseInfo friendBaseInfo;
+//	private GameObject itemLeft;
+//    private UIImageButton startQuestBtn;
+//	private UILabel rightIndexLabel;
 //	private UIButton prePageButton;
 //	private UIButton nextPageButton;
-    private int currentPartyIndex;
-    private int partyTotalCount;
-
-	private UIButton sortBtn;
-	private UILabel sortRuleLabel;
-	private SortRule curSortRule;
-
-	private TFriendInfo selectedHelper;
-	private uint questID;
-    private uint stageID;
-	private TEvolveStart evolveStart = null;
-	private Dictionary<int, PageUnitItem> partyView = new Dictionary<int, PageUnitItem>();
-
+//    private int currentPartyIndex;
+//    private int partyTotalCount;
+//	private TFriendInfo selectedHelper;
+//	private uint questID;
+//    private uint stageID;
+//	private TEvolveStart evolveStart = null;
+//	private Dictionary<int, PageUnitItem> partyView = new Dictionary<int, PageUnitItem>();
 	private List<TFriendInfo> helperDataList = new List<TFriendInfo>();
-
 	public override void Init(UIInsConfig config, IUICallback origin) {
 		base.Init(config, origin);
-//		InitUI();
+		InitUI();
 	}
 	
 	public override void ShowUI() {
 		base.ShowUI();
-		MsgCenter.Instance.AddListener(CommandEnum.OnPickQuest, RecordPickedInfoForFight);
+		AddCmdListener();
 		CreateDragView();
 		ShowUIAnimation();
-
 //		SetBottomButtonActive(false);
 //		prevPosition = -1;
 //		AddCommandListener();
@@ -43,35 +34,14 @@ public class FriendHelperView : UIComponentUnity{
 //		MsgCenter.Instance.Invoke(CommandEnum.RefreshPartyPanelInfo, curParty);
 	}
 
-	private void ShowUIAnimation(){
-		gameObject.transform.localPosition = new Vector3(-1000, 0, 0);
-		iTween.MoveTo(gameObject, iTween.Hash("x", 0, "time", 0.4f));       
-	}
-
-	void AddCommandListener(){
-		MsgCenter.Instance.AddListener(CommandEnum.ChooseHelper, ChooseHelper);
-		MsgCenter.Instance.AddListener(CommandEnum.GetSelectedQuest, RecordSelectedQuest);
-		MsgCenter.Instance.AddListener(CommandEnum.SortByRule, ReceiveSortInfo);
-	}
-
-	void RemoveCommandListener(){
-		MsgCenter.Instance.RemoveListener(CommandEnum.ChooseHelper, ChooseHelper);
-		MsgCenter.Instance.RemoveListener(CommandEnum.GetSelectedQuest, RecordSelectedQuest);
-		MsgCenter.Instance.RemoveListener(CommandEnum.SortByRule, ReceiveSortInfo);
-	}
-
 	public override void HideUI() {
 		base.HideUI();
-//		SetBottomButtonActive(false);
-//		RemoveCommandListener();
 		dragPanel.DestoryUI();
-		MsgCenter.Instance.RemoveListener(CommandEnum.OnPickQuest, RecordPickedInfoForFight);
+		RmvCmdListener();
 	}
 
 	public override void DestoryUI () {
 		base.DestoryUI ();
-		RemoveCommandListener();
-		MsgCenter.Instance.RemoveListener(CommandEnum.OnPickQuest, RecordPickedInfoForFight);
 	}
 
 //	public override void CallbackView(object data){
@@ -131,219 +101,210 @@ public class FriendHelperView : UIComponentUnity{
 //		InitPagePanel();
 //    }
 
-	void UpdateViewAfterChooseHelper(){
-		startQuestBtn.isEnabled = true;
-		UIEventListener.Get(startQuestBtn.gameObject).onClick = ClickBottomButton;
-		LightClickItem();
-	}
+//	void UpdateViewAfterChooseHelper(){
+//		startQuestBtn.isEnabled = true;
+//		UIEventListener.Get(startQuestBtn.gameObject).onClick = ClickBottomButton;
+//		LightClickItem();
+//	}
 
-	int prevPosition = -1;
-	UISprite prevSprite;
+//	int prevPosition = -1;
+//	UISprite prevSprite;
+//
+//	void LightClickItem(){
+////		Debug.LogError("FriendHelperView.LightClickItem()...");
+//		if(prevPosition == -1) return;
+//		GameObject pickedItem = dragPanel.ScrollItem[ prevPosition ];
+//		UISprite lightSpr = pickedItem.transform.FindChild("Sprite_Light").GetComponent<UISprite>();
+//		if(lightSpr == null) {
+//			Debug.LogError("lightSpr is null");
+//			return;
+//		}
+//		if(prevSprite != null) {
+//			if(lightSpr.Equals( prevSprite))
+//				return;
+//			else
+//				prevSprite.enabled = false;
+//		}
+//		
+//		lightSpr.enabled = true;
+//		prevSprite = lightSpr;
+//	}
 
-	void LightClickItem(){
-//		Debug.LogError("FriendHelperView.LightClickItem()...");
-		if(prevPosition == -1) return;
-		GameObject pickedItem = dragPanel.ScrollItem[ prevPosition ];
-		UISprite lightSpr = pickedItem.transform.FindChild("Sprite_Light").GetComponent<UISprite>();
-		if(lightSpr == null) {
-			Debug.LogError("lightSpr is null");
-			return;
-		}
-		if(prevSprite != null) {
-			if(lightSpr.Equals( prevSprite))
-				return;
-			else
-				prevSprite.enabled = false;
-		}
-		
-		lightSpr.enabled = true;
-		prevSprite = lightSpr;
-	}
+//	void ClickBottomButton(GameObject btn){
+//		startQuestBtn.isEnabled = false;
+//		QuestStart();
+//	}
+//
+//	void SetBottomButtonActive(bool active){
+//		startQuestBtn.isEnabled = active;
+//	}
+//
+//	void AddHelperItem(TFriendInfo tfi ){
+//		Texture2D tex = tfi.UserUnit.UnitInfo.GetAsset(UnitAssetType.Avatar);
+////		Debug.LogError ("AddHelperItem : " + itemLeft + " time : " + Time.realtimeSinceStartup);
+//		if (itemLeft == null) {
+//			FindItemLeft();
+//		}
+//		UITexture uiTexture = itemLeft.transform.FindChild("Texture").GetComponent<UITexture>();
+//		uiTexture.mainTexture = tex;
+//	}
 
-	void ClickBottomButton(GameObject btn){
-		startQuestBtn.isEnabled = false;
-		QuestStart();
-	}
+//	void FindItemLeft(){
+//		itemLeft = transform.FindChild("Item_Left").gameObject;
+////		Debug.LogError ("FindItemLeft : " + itemLeft + " time : " + Time.realtimeSinceStartup);
+//	}
 
-	void SetBottomButtonActive(bool active){
-		startQuestBtn.isEnabled = active;
-	}
+//	private void InitPagePanel(){
+//		rightIndexLabel = FindChild<UILabel>("Label_Cur_Party");
+////		prePageButton = FindChild<UIButton>("Button_Left");
+////		UIEventListener.Get(prePageButton.gameObject).onClick = PrevPage;
+////		nextPageButton = FindChild<UIButton>("Button_Right");
+////		UIEventListener.Get(nextPageButton.gameObject).onClick = NextPage;
+//		
+//		for (int i = 0; i < 4; i++){
+//			PageUnitItem puv = FindChild<PageUnitItem>(i.ToString());
+//			partyView.Add(i, puv);
+//		}
+//	}
 
-	void AddHelperItem(TFriendInfo tfi ){
-		Texture2D tex = tfi.UserUnit.UnitInfo.GetAsset(UnitAssetType.Avatar);
-//		Debug.LogError ("AddHelperItem : " + itemLeft + " time : " + Time.realtimeSinceStartup);
-		if (itemLeft == null) {
-			FindItemLeft();
-		}
-		UITexture uiTexture = itemLeft.transform.FindChild("Texture").GetComponent<UITexture>();
-		uiTexture.mainTexture = tex;
-	}
+//	void PrevPage(GameObject go){
+//		TUnitParty preParty = DataCenter.Instance.PartyInfo.PrevParty;
+//		RefreshParty(preParty);  
+//		MsgCenter.Instance.Invoke(CommandEnum.RefreshPartyPanelInfo, preParty);         
+//	}
+//
+//	void NextPage(GameObject go){
+//		TUnitParty nextParty = DataCenter.Instance.PartyInfo.NextParty;
+//		RefreshParty(nextParty);
+//		MsgCenter.Instance.Invoke(CommandEnum.RefreshPartyPanelInfo, nextParty);
+//	}
 
-	void FindItemLeft(){
-		itemLeft = transform.FindChild("Item_Left").gameObject;
-//		Debug.LogError ("FindItemLeft : " + itemLeft + " time : " + Time.realtimeSinceStartup);
-	}
-
-	private void InitPagePanel(){
-		rightIndexLabel = FindChild<UILabel>("Label_Cur_Party");
-//		prePageButton = FindChild<UIButton>("Button_Left");
-//		UIEventListener.Get(prePageButton.gameObject).onClick = PrevPage;
-//		nextPageButton = FindChild<UIButton>("Button_Right");
-//		UIEventListener.Get(nextPageButton.gameObject).onClick = NextPage;
-		
-		for (int i = 0; i < 4; i++){
-			PageUnitItem puv = FindChild<PageUnitItem>(i.ToString());
-			partyView.Add(i, puv);
-		}
-	}
-
-	void PrevPage(GameObject go){
-		TUnitParty preParty = DataCenter.Instance.PartyInfo.PrevParty;
-		RefreshParty(preParty);  
-		MsgCenter.Instance.Invoke(CommandEnum.RefreshPartyPanelInfo, preParty);         
-	}
-
-	void NextPage(GameObject go){
-		TUnitParty nextParty = DataCenter.Instance.PartyInfo.NextParty;
-		RefreshParty(nextParty);
-		MsgCenter.Instance.Invoke(CommandEnum.RefreshPartyPanelInfo, nextParty);
-	}
-
-	void RefreshParty(TUnitParty party){
-		List<TUserUnit> partyMemberList = party.GetUserUnit();
-		int curPartyIndex = DataCenter.Instance.PartyInfo.CurrentPartyId + 1;
-		rightIndexLabel.text = curPartyIndex.ToString();	
-		//Debug.Log("Current party's member count is : " + partyMemberList.Count);
-		for (int i = 0; i < partyMemberList.Count; i++){
-			partyView[ i ].Init(partyMemberList [ i ]);
-		}
-	}
-
-	private void ClickSortBtn(GameObject btn){
-		MsgCenter.Instance.Invoke(CommandEnum.OpenSortRuleWindow, true);
-	}
-
-	private void ReceiveSortInfo(object msg){
-		//Debug.LogError("FriendHelper.ReceiveSortInfo()...");
-		curSortRule = (SortRule)msg;
-		SortUnitByCurRule();
-	}
-
-	private void SortUnitByCurRule(){
-		sortRuleLabel.text = curSortRule.ToString();
-		SortUnitTool.SortByTargetRule(curSortRule, helperDataList);
-
-		for (int i = 0; i < dragPanel.ScrollItem.Count; i++){
-			HelperUnitItem huv = dragPanel.ScrollItem[ i ].GetComponent<HelperUnitItem>();
-			//Debug.Log(string.Format("SortUnitByCurRule :: Before:: ScrollItem's index -> {0}, huv's addPoint -> {1}", i, huv.UserUnit.AddNumber));
-			huv.UserUnit = helperDataList[ i ].UserUnit;
-			huv.CurrentSortRule = curSortRule;
-		}
-	}
-
-	void ShowHelperInfo(int pos){
-		AudioManager.Instance.PlayAudio(AudioEnum.sound_click);
-		RecordSelectedHelper(helperDataList[ pos ]);
-		MsgCenter.Instance.Invoke(CommandEnum.FriendBriefInfoShow, helperDataList[ pos ]);
-	}
+//	void RefreshParty(TUnitParty party){
+//		List<TUserUnit> partyMemberList = party.GetUserUnit();
+//		int curPartyIndex = DataCenter.Instance.PartyInfo.CurrentPartyId + 1;
+//		rightIndexLabel.text = curPartyIndex.ToString();	
+//		//Debug.Log("Current party's member count is : " + partyMemberList.Count);
+//		for (int i = 0; i < partyMemberList.Count; i++){
+//			partyView[ i ].Init(partyMemberList [ i ]);
+//		}
+//	}
 	
-	void RecordSelectedHelper(TFriendInfo tfi){
-		selectedHelper = tfi;
-	}
-
-	void ChooseHelper(object msg){
-		if(selectedHelper == null) { 
-			Debug.Log("selectedHelper is NULL, return...");
-			return;
-		}
-		AddHelperItem(selectedHelper);
-		UpdateViewAfterChooseHelper();
-	}
+//	void ShowHelperInfo(int pos){
+//		AudioManager.Instance.PlayAudio(AudioEnum.sound_click);
+//		RecordSelectedHelper(helperDataList[ pos ]);
+//		MsgCenter.Instance.Invoke(CommandEnum.FriendBriefInfoShow, helperDataList[ pos ]);
+//	}
 	
-	private void QuestStart(){
-		AudioManager.Instance.PlayAudio(AudioEnum.sound_click);
-		
-		if (DataCenter.gameStage == GameState.Evolve) {
-			evolveStart.EvolveStart.restartNew = 1;
-			evolveStart.EvolveStart.OnRequest(null, RspEvolveStartQuest);
-		} 
-		else {
-			StartQuest sq = new StartQuest ();
-			StartQuestParam sqp = new StartQuestParam ();
-			sqp.currPartyId = DataCenter.Instance.PartyInfo.CurrentPartyId;
-			sqp.helperUserUnit = selectedHelper;
-			sqp.questId = questID;
-			sqp.stageId = stageID;
-			sqp.startNew = 1;
-			sq.OnRequest (sqp, RspStartQuest);
-		}
-	}
+//	void RecordSelectedHelper(TFriendInfo tfi){
+//		selectedHelper = tfi;
+//	}
+//
+//	void ChooseHelper(object msg){
+//		if(selectedHelper == null) { 
+//			Debug.Log("selectedHelper is NULL, return...");
+//			return;
+//		}
+//		AddHelperItem(selectedHelper);
+//		UpdateViewAfterChooseHelper();
+//	}
+//	
+//	private void QuestStart(){
+//		AudioManager.Instance.PlayAudio(AudioEnum.sound_click);
+//		
+//		if (DataCenter.gameStage == GameState.Evolve) {
+//			evolveStart.EvolveStart.restartNew = 1;
+//			evolveStart.EvolveStart.OnRequest(null, RspEvolveStartQuest);
+//		} 
+//		else {
+//			StartQuest sq = new StartQuest ();
+//			StartQuestParam sqp = new StartQuestParam ();
+//			sqp.currPartyId = DataCenter.Instance.PartyInfo.CurrentPartyId;
+//			sqp.helperUserUnit = selectedHelper;
+//			sqp.questId = questID;
+//			sqp.stageId = stageID;
+//			sqp.startNew = 1;
+//			sq.OnRequest (sqp, RspStartQuest);
+//		}
+//	}
+//
+//	void RecordSelectedQuest(object msg){
+//		Dictionary<string,uint> idArgs = msg as Dictionary<string,uint>;
+//		questID = idArgs["QuestID"];
+//		stageID = idArgs["StageID"];
+//	}
 
-	void RecordSelectedQuest(object msg){
-		Dictionary<string,uint> idArgs = msg as Dictionary<string,uint>;
-		questID = idArgs["QuestID"];
-		stageID = idArgs["StageID"];
-	}
-
-	void RspEvolveStartQuest (object data) {
-		if (data == null){
-			//	Debug.Log("OnRspEvolveStart(), response null");
-			return;
-		}
-		evolveStart.StoreData ();
-		
-		bbproto.RspEvolveStart rsp = data as bbproto.RspEvolveStart;
-		if (rsp.header.code != (int)ErrorCode.SUCCESS) {
-			Debug.LogError("Rsp code: "+rsp.header.code+", error:"+rsp.header.error);
-			ErrorMsgCenter.Instance.OpenNetWorkErrorMsgWindow(rsp.header.code);
-			return;
-		}
-		// TODO do evolve start over;
-		DataCenter.Instance.UserInfo.StaminaNow = rsp.staminaNow;
-		DataCenter.Instance.UserInfo.StaminaRecover = rsp.staminaRecover;
-		bbproto.QuestDungeonData questDungeonData = rsp.dungeonData;
-		TQuestDungeonData tqdd = new TQuestDungeonData (questDungeonData);
-		ModelManager.Instance.SetData(ModelEnum.MapConfig, tqdd);
-		
-		EnterBattle ();
-	}
+//	void RspEvolveStartQuest (object data) {
+//		if (data == null){
+//			//	Debug.Log("OnRspEvolveStart(), response null");
+//			return;
+//		}
+//		evolveStart.StoreData ();
+//		
+//		bbproto.RspEvolveStart rsp = data as bbproto.RspEvolveStart;
+//		if (rsp.header.code != (int)ErrorCode.SUCCESS) {
+//			Debug.LogError("Rsp code: "+rsp.header.code+", error:"+rsp.header.error);
+//			ErrorMsgCenter.Instance.OpenNetWorkErrorMsgWindow(rsp.header.code);
+//			return;
+//		}
+//		// TODO do evolve start over;
+//		DataCenter.Instance.UserInfo.StaminaNow = rsp.staminaNow;
+//		DataCenter.Instance.UserInfo.StaminaRecover = rsp.staminaRecover;
+//		bbproto.QuestDungeonData questDungeonData = rsp.dungeonData;
+//		TQuestDungeonData tqdd = new TQuestDungeonData (questDungeonData);
+//		ModelManager.Instance.SetData(ModelEnum.MapConfig, tqdd);
+//		
+//		EnterBattle ();
+//	}
 	
-	void RspStartQuest(object data) {
-		TQuestDungeonData tqdd = null;
-		bbproto.RspStartQuest rspStartQuest = data as bbproto.RspStartQuest;
-		if (rspStartQuest.header.code != (int)ErrorCode.SUCCESS) {
-			Debug.LogError("Rsp code: "+rspStartQuest.header.code+", error:"+rspStartQuest.header.error);
-			ErrorMsgCenter.Instance.OpenNetWorkErrorMsgWindow(rspStartQuest.header.code);
-			return;
-		}
-		//Debug.LogError (rspStartQuest.header.code  + "  " + rspStartQuest.header.error);
-		if (rspStartQuest.header.code == 0 && rspStartQuest.dungeonData != null) {
-			LogHelper.Log("rspStartQuest code:{0}, error:{1}", rspStartQuest.header.code, rspStartQuest.header.error);
-			DataCenter.Instance.UserInfo.StaminaNow = rspStartQuest.staminaNow;
-			DataCenter.Instance.UserInfo.StaminaRecover = rspStartQuest.staminaRecover;
-			tqdd = new TQuestDungeonData(rspStartQuest.dungeonData);
-			ModelManager.Instance.SetData(ModelEnum.MapConfig, tqdd);
-		}
-		
-		if (data == null || tqdd == null) {
-			//Debug.LogError("Request quest info fail : data " + data + "  TQuestDungeonData : " + tqdd);
-			return;
-		}
-		EnterBattle ();
-	} 
+//	void RspStartQuest(object data) {
+//		TQuestDungeonData tqdd = null;
+//		bbproto.RspStartQuest rspStartQuest = data as bbproto.RspStartQuest;
+//		if (rspStartQuest.header.code != (int)ErrorCode.SUCCESS) {
+//			Debug.LogError("Rsp code: "+rspStartQuest.header.code+", error:"+rspStartQuest.header.error);
+//			ErrorMsgCenter.Instance.OpenNetWorkErrorMsgWindow(rspStartQuest.header.code);
+//			return;
+//		}
+//		//Debug.LogError (rspStartQuest.header.code  + "  " + rspStartQuest.header.error);
+//		if (rspStartQuest.header.code == 0 && rspStartQuest.dungeonData != null) {
+//			LogHelper.Log("rspStartQuest code:{0}, error:{1}", rspStartQuest.header.code, rspStartQuest.header.error);
+//			DataCenter.Instance.UserInfo.StaminaNow = rspStartQuest.staminaNow;
+//			DataCenter.Instance.UserInfo.StaminaRecover = rspStartQuest.staminaRecover;
+//			tqdd = new TQuestDungeonData(rspStartQuest.dungeonData);
+//			ModelManager.Instance.SetData(ModelEnum.MapConfig, tqdd);
+//		}
+//		
+//		if (data == null || tqdd == null) {
+//			//Debug.LogError("Request quest info fail : data " + data + "  TQuestDungeonData : " + tqdd);
+//			return;
+//		}
+//		EnterBattle ();
+//	} 
 	
-	void EnterBattle () {
-		DataCenter.Instance.BattleFriend = selectedHelper;
-		UIManager.Instance.EnterBattle();
-	} 
+//	void EnterBattle () {
+//		DataCenter.Instance.BattleFriend = selectedHelper;
+//		UIManager.Instance.EnterBattle();
+//	} 
+//	
+//	MsgWindowParams GetStartQuestError () {
+//		MsgWindowParams mwp = new MsgWindowParams ();
+//		return mwp;
+//	}
 	
-	MsgWindowParams GetStartQuestError () {
-		MsgWindowParams mwp = new MsgWindowParams ();
-		return mwp;
-	}
-
-
 	//----------------------------New-------------------------------
+	private DragPanel dragPanel;
+	private UIButton sortBtn;
+	private UILabel sortRuleLabel;
+	private SortRule curSortRule;
+
+	private void InitUI(){
+		sortBtn = transform.FindChild("Button_Sort").GetComponent<UIButton>();
+		UIEventListener.Get(sortBtn.gameObject).onClick = ClickSortBtn;
+		sortRuleLabel = transform.FindChild("Label_Sort_Rule").GetComponent<UILabel>();
+
+		curSortRule = SortUnitTool.DEFAULT_SORT_RULE;
+		sortRuleLabel.text = curSortRule.ToString();
+	}
+
 	private void CreateDragView(){
 		helperDataList = DataCenter.Instance.SupportFriends;
 		dragPanel = new DragPanel("FriendHelperDragPanel", HelperUnitItem.ItemPrefab);
@@ -356,9 +317,9 @@ public class FriendHelperView : UIComponentUnity{
 			huv.Init(helperDataList[ i ]);
 			huv.callback = ClickHelperItem;
 		}
-		//SortUnitByCurRule();
+		SortUnitByCurRule();
 	}
-
+	
 	private QuestItemView pickedQuestInfo;
 	private void RecordPickedInfoForFight(object msg){
 		Debug.Log("FriendHelper.RecordPickedInfoForFight(), received info...");
@@ -379,6 +340,43 @@ public class FriendHelperView : UIComponentUnity{
 
 		UIManager.Instance.ChangeScene(SceneEnum.StandBy);//before
 		MsgCenter.Instance.Invoke(CommandEnum.OnPickHelper, pickedInfo);//after
+	}
+
+	private void ClickSortBtn(GameObject btn){
+		MsgCenter.Instance.Invoke(CommandEnum.OpenSortRuleWindow, true);
+	}
+	
+	private void ReceiveSortInfo(object msg){
+		//Debug.LogError("FriendHelper.ReceiveSortInfo()...");
+		curSortRule = (SortRule)msg;
+		SortUnitByCurRule();
+	}
+	
+	private void SortUnitByCurRule(){
+		sortRuleLabel.text = curSortRule.ToString();
+		SortUnitTool.SortByTargetRule(curSortRule, helperDataList);
+		
+		for (int i = 0; i < dragPanel.ScrollItem.Count; i++){
+			HelperUnitItem huv = dragPanel.ScrollItem[ i ].GetComponent<HelperUnitItem>();
+			//Debug.Log(string.Format("SortUnitByCurRule :: Before:: ScrollItem's index -> {0}, huv's addPoint -> {1}", i, huv.UserUnit.AddNumber));
+			huv.UserUnit = helperDataList[ i ].UserUnit;
+			huv.CurrentSortRule = curSortRule;
+		}
+	}
+
+	private void ShowUIAnimation(){
+		gameObject.transform.localPosition = new Vector3(-1000, 0, 0);
+		iTween.MoveTo(gameObject, iTween.Hash("x", 0, "time", 0.4f));       
+	}
+	
+	private void AddCmdListener(){
+		MsgCenter.Instance.AddListener(CommandEnum.OnPickQuest, RecordPickedInfoForFight);
+		MsgCenter.Instance.AddListener(CommandEnum.SortByRule, ReceiveSortInfo);
+	}
+
+	private void RmvCmdListener(){
+		MsgCenter.Instance.RemoveListener(CommandEnum.OnPickQuest, RecordPickedInfoForFight);
+		MsgCenter.Instance.RemoveListener(CommandEnum.SortByRule, ReceiveSortInfo);
 	}
 
 	    
