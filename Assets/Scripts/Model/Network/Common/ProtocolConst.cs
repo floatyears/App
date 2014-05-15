5d54e99c53523ffc3e1fc0bbdd1ce8052d730543
kory <119238@qq.com>
2014-05-15 21:54:39 +0800

    update proto

--- Assets/Scripts/Model/Network/Common/ProtocolConst.cs
+++ Assets/Scripts/Model/Network/Common/ProtocolConst.cs
@@ -6,6 +6,7 @@ class Protocol {
     public const string AUTH_USER = "auth_user";
     public const string RENAME_NICK = "rename_nick";
 	public const string CHANGE_PARTY = "change_party";
+	public const string FINISH_USER_GUIDE = "finish_user_guide";
 
     public const string START_QUEST = "start_quest";
     public const string CLEAR_QUEST = "clear_quest";
