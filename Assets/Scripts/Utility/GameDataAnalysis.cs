32f176324c29e7e52291c271f51c310e2fb907eb
floatyears <cl735915647@gmail.com>
2014-05-16 13:54:50 +0800

    友盟统计封装

--- Assets/Scripts/Utility/GameDataAnalysis.cs
+++ Assets/Scripts/Utility/GameDataAnalysis.cs
@@ -7,20 +7,47 @@ public static class GameDataAnalysis {
 
 	public static Dictionary<GameDataAnalysisEventType,int> EventTime = new Dictionary<GameDataAnalysisEventType, int>();
 
-	public static void Event(GameDataAnalysisEventType eventId,string? key = null,float? value1 = null, int? value2 = null,string? value3 = null,Dictionary<string, string> value = null){
-		GA.Event (eventId.ToString(),key);
+	public static void Event(GameDataAnalysisEventType eventId){
+		GA.Event (eventId.ToString());	
+		
 	}
 
-	public static void EventBegin(GameDataAnalysisEventType eventId,string? key = null,float? value1 = null, int? value2 = null,string? value3 = null,Dictionary<string, string> value = null){
-		EventTime [GameDataAnalysisEventType] = TimeHelper.MillionSecondsNow();
+	public static void Event(GameDataAnalysisEventType eventId, string key){
+		GA.Event(eventId.ToString(),key)
 	}
 
-	public static void EventEnd(GameDataAnalysisEventType eventId,string? key = null,float? value1 = null, int? value2 = null,string? value3 = null,Dictionary<string, string> value = null){
-		EventTime [GameDataAnalysisEventType] = TimeHelper.MillionSecondsNow();
+	public static void Event(GameDataAnalysisEventType eventId, Dictionary<string,string> attr){
+		GA.Event (eventId.ToString (), attr);
+	}
+
+	
+	public static void EventCount(GameDataAnalysisEventType eventId,string key,Dictionary<string,string> attr){
+		GA.Event (eventId.ToString (), key, attr);
+	}
+
+	public static void EventCount(GameDataAnalysisEventType eventId, Dictionary<string,string> attr, int count){
+		GA.Event (eventId.ToString (), attr, count);
+	}
+
+
+	public static void EventBegin(GameDataAnalysisEventType eventId){
+		EventTime [eventId] = TimeHelper.MillionSecondsNow();
+	}
+
+	public static void EventEnd(GameDataAnalysisEventType eventId,Dictionary<strig,string>value){
+		EventTime [eventId] = TimeHelper.MillionSecondsNow();
+	}
+
+	public static void Buy(string item,int amount,double price){
+		GA.Buy(item,amount,price);
+	}
+
+	public static void Pay(double cash,GA.PaySource source,double coin){
+		GA.Pay(cash,source,coin);
 	}
 	
 }
 
 public enum GameDataAnalysisEventType{
-
+	Login
 }
