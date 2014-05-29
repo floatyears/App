4a6abf83e8b9b445a3fab237768c0f97069cfb01
kory <119238@qq.com>
2014-05-30 02:10:26 +0800

    set City1.pos

--- Server/protobuf/stats/make.sh
+++ Server/protobuf/stats/make.sh
@@ -5,10 +5,15 @@ cat ../*.proto|grep -v "^import.*"|sed 's/^package.*/\/\/=======================
 cat ./stats.proto >> $OUT_FILE
 ls -lrt
 
-protoc --go_out=../../src/stats stats_all.proto
+stat_path="../../src/stats"
+protoc --go_out=$stat_path stats_all.proto
+
+cd ../client_proto/
+protoc --go_out=$stat_path skill.proto bbproto.proto
+cd -
 
 #cp files to stats dir
-cp ../../src/stats/stats_all.pb.go ../../../../bbmstats/src/stats/
+cp $stat_path/stats_all.pb.go ../../../../bbmstats/src/stats/
 cp ./stats.proto ../../../../bbmstats/src/stats/
 
 ls -lrt ../../../bbsvr/src/stats/stats_all.pb.go
