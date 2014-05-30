0eef0b608342dc6f74da5bf5523c120f3a63ec86
floatyears <cl735915647@gmail.com>
2014-05-30 12:09:17 +0800

    unit detail bug fix

---
+++ Assets/Scripts/Effect/TweenAnimation/LoadingRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingRotate : MonoBehaviour {
+
+	public Vector3 rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.rotation *= Quaternion.Euler (rotateSpeed);
+		//transform.rotation = Quaternion.Euler (new Vector3(transform.rotation.x + rotateSpeed.x,transform.rotation.y + rotateSpeed.y,transform.rotation.z + rotateSpeed.z));
+	}
+}
