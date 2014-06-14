2fa209e3fb7c3c6ea4b6f68bf36541962ce2625e
kory <119238@qq.com>
2014-06-14 17:15:30 +0800

    config trap id.

--- Assets/Scripts/Model/Map/ConfigTrap.cs
+++ Assets/Scripts/Model/Map/ConfigTrap.cs
@@ -7,88 +7,113 @@ public class ConfigTrap {
 		ConfigTrapInfo ();
 	}
 
-	public void ConfigTrapInfo () {
+	public void ConfigTrapMove () {
 		TrapInfo ti = new TrapInfo ();
-		ti.trapID = 1;
+		ti.trapID = 11; // type1, lv1
 		ti.trapType = ETrapType.Move;
 		ti.effectType = 1;	// 1: blocking. 2: teleporter. 3: restart.
 		MoveTrap mt = new MoveTrap (ti);
 		if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
-						DataCenter.Instance.TrapInfo [ti.trapID] = mt;
-				} else {
+			DataCenter.Instance.TrapInfo [ti.trapID] = mt;
+		} else {
 			DataCenter.Instance.TrapInfo.Add (ti.trapID, mt);
 		}
 
-
 		ti = new TrapInfo ();
-		ti.trapID = 5;
+		ti.trapID = 23; //type2, lv=3
 		ti.trapType = ETrapType.Move;
-		ti.effectType = 2;	// 1: blocking. 2: teleporter. 3: restart.
+		ti.effectType = 2;	// 2: teleporter
 		mt = new MoveTrap (ti);
-
 		if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
 			DataCenter.Instance.TrapInfo [ti.trapID] = mt;
 		} else {
 			DataCenter.Instance.TrapInfo.Add (ti.trapID, mt);
 		}
 
-//		DataCenter.Instance.TrapInfo.Add (ti.trapID, mt);
-
 		ti = new TrapInfo ();
-		ti.trapID = 6;
+		ti.trapID = 94; //type9, lv=4
 		ti.trapType = ETrapType.Move;
-		ti.effectType = 3;	// 1: blocking. 2: teleporter. 3: restart.
+		ti.effectType = 3;	// 3: restart: move back to start point
 		mt = new MoveTrap (ti);
-
 		if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
 			DataCenter.Instance.TrapInfo [ti.trapID] = mt;
 		} else {
 			DataCenter.Instance.TrapInfo.Add (ti.trapID, mt);
 		}
 
-//		DataCenter.Instance.TrapInfo.Add (ti.trapID, mt);
+	}
 
-		ti = new TrapInfo ();
-		ti.trapID = 2;
-		ti.trapType = ETrapType.Injured;
-		ti.effectType = 1;
-		ti.valueIndex = 1;
-		InjuredTrap it = new InjuredTrap (ti);
+	public void ConfigTrapInjured (int effectType) {
+		// effectType = 1, 2, 3, 4
+		for(uint index=0; index<10; index++) {
+			TrapInfo ti = new TrapInfo ();
+			ti.effectType = effectType;
+			ti.valueIndex = (int)index;
 
-		if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
-			DataCenter.Instance.TrapInfo [ti.trapID] = it;
-		} else {
-			DataCenter.Instance.TrapInfo.Add (ti.trapID, it);
+			uint trapid = 2 + (uint)effectType; //convert: 1,2,3,4 -> 3, 4, 5, 6
+			ti.trapID = trapid*10 + index;
+			ti.trapType = ETrapType.Injured;
+
+			InjuredTrap it = new InjuredTrap (ti);
+			if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
+				DataCenter.Instance.TrapInfo [ti.trapID] = it;
+			} else {
+				DataCenter.Instance.TrapInfo.Add (ti.trapID, it);
+			}
+		}
+	}
+
+	public void ConfigTrapChangeEnvir () {
+		uint trapid = 7;
+
+		for(uint index=0; index<10; index++) {
+			TrapInfo ti = new TrapInfo ();
+			ti.trapID = trapid*10 + index;
+			ti.trapType = ETrapType.ChangeEnvir;
+			ti.valueIndex =  (int)index;
+			EnvironmentTrap et = new EnvironmentTrap (ti);
+			
+			if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
+				DataCenter.Instance.TrapInfo [ti.trapID] = et;
+			} else {
+				DataCenter.Instance.TrapInfo.Add (ti.trapID, et);
+			}
+		}
+	}
+	
+	public void ConfigTrapStateException () {
+		uint trapid = 8;
+		for(uint index=0; index<10; index++) {
+			TrapInfo ti = new TrapInfo ();
+			ti.trapID = trapid*10 + index;
+			ti.trapType = ETrapType.StateException;
+			ti.valueIndex =  (int)index;
+			ti.effectType = 5; //PS: use effectType as round=5 for Poison
+			TrapPosion tp = new TrapPosion (ti);
+
+			if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
+				DataCenter.Instance.TrapInfo [ti.trapID] = tp;
+			} else {
+				DataCenter.Instance.TrapInfo.Add (ti.trapID, tp);
+			}
+		}
+	}
+
+	public void ConfigTrapInfo () {
+		ConfigTrapMove ();
+
+		//TrapInjured effectType:
+		//		mineInfo		= 1;
+		//		trappingInfo	= 2;
+		//		hungryInfo		= 3;
+		//		lostMoney		= 4;
+		for(int effectType = 1; effectType <= 4; effectType++) {
+			ConfigTrapInjured( effectType );
 		}
 
-//		DataCenter.Instance.TrapInfo.Add (ti.trapID, it);
+		ConfigTrapStateException();
 
-		ti = new TrapInfo ();
-		ti.trapID = 3;
-		ti.trapType = ETrapType.StateException;
-		ti.valueIndex = 1;
-		ti.effectType = 5; //use effectType as round=5 for Poison
-		TrapPosion tp = new TrapPosion (ti);
-//		DataCenter.Instance.TrapInfo.Add (ti.trapID, tp);
-
-		if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
-			DataCenter.Instance.TrapInfo [ti.trapID] = tp;
-		} else {
-			DataCenter.Instance.TrapInfo.Add (ti.trapID, tp);
-		}
-
-		ti = new TrapInfo ();
-		ti.trapID = 4;
-		ti.trapType = ETrapType.ChangeEnvir;
-		ti.valueIndex = 2;
-		EnvironmentTrap et = new EnvironmentTrap (ti);
-//		DataCenter.Instance.TrapInfo.Add (ti.trapID, et);
-
-		if (DataCenter.Instance.TrapInfo.ContainsKey (ti.trapID)) {
-			DataCenter.Instance.TrapInfo [ti.trapID] = et;
-		} else {
-			DataCenter.Instance.TrapInfo.Add (ti.trapID, et);
-		}
+		ConfigTrapChangeEnvir();
 	}
 
 }
