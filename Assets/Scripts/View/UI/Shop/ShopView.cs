dbaf3ded153d611d6ddfe0fe27211da5461b5b0a
floatyears <cl735915647@gmail.com>
2014-07-09 22:04:25 +0800

    pay

--- Assets/Scripts/View/UI/Shop/ShopView.cs
+++ Assets/Scripts/View/UI/Shop/ShopView.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Soomla;
 
 public class ShopView : UIComponentUnity {
 	private Dictionary<string,UIButton> buttonDic = new Dictionary<string, UIButton>();
@@ -10,18 +11,32 @@ public class ShopView : UIComponentUnity {
 	private GameObject infoPanelRoot;
 	private GameObject windowRoot;
 
+	private GameCurrencyEventHandler handler;
+
 	public override void Init ( UIInsConfig config, IUICallback origin ) {
 		base.Init (config, origin);
 		InitUI();
+
+		handler = new GameCurrencyEventHandler ();
+		
+		StoreController.Initialize (new GameCurrencyAssets ());
 	}
 	
 	public override void ShowUI () {
 		base.ShowUI ();
 		ShowUIAnimation();
+
+		#if UNITY_ANDROID
+		StoreController.StartIabServiceInBg();
+		#endif
 	}
 	
 	public override void HideUI () {
 		base.HideUI ();
+
+		#if UNITY_ANDROID
+		StoreController.StopIabServiceInBg();
+		#endif
 	}
 	
 	public override void DestoryUI () {
@@ -82,5 +97,29 @@ public class ShopView : UIComponentUnity {
 		windowRoot.transform.localPosition = new Vector3(1000, -620, 0);
 		iTween.MoveTo(infoPanelRoot, iTween.Hash("x", 0, "time", 0.4f, "islocal", true));
 		iTween.MoveTo(windowRoot, iTween.Hash("x", 0, "time", 0.4f, "islocal", true));
+	}
+
+	public void Buy1(){
+		StoreInventory.BuyItem (GameCurrencyAssets.CHIP_PACK1.ItemId);
+	}
+	
+	public void Buy2(){
+		StoreInventory.BuyItem (GameCurrencyAssets.CHIP_PACK2.ItemId);
+	}
+
+	public void Buy3(){
+		StoreInventory.BuyItem (GameCurrencyAssets.CHIP_PACK3.ItemId);
+	}
+
+	public void Buy4(){
+		StoreInventory.BuyItem (GameCurrencyAssets.CHIP_PACK4.ItemId);
+	}
+
+	public void Buy5(){
+		StoreInventory.BuyItem (GameCurrencyAssets.CHIP_PACK5.ItemId);
+	}
+
+	public void Buy6(){
+		StoreInventory.BuyItem (GameCurrencyAssets.CHIP_PACK6.ItemId);
 	}
 }
