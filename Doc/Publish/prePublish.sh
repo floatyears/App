645508d9fcf01f87e4dded4af11d0c459315510a
kory <119238@qq.com>
2014-07-24 11:10:47 +0800

    no message

---
+++ Doc/Publish/prePublish.sh
@@ -0,0 +1,10 @@
+language=$1
+rootpath=../../
+destFont="${rootpath}/Assets/Resources/Font/Dimbo Regular.ttf"
+if [ "${language}" = "cn" ]; then
+	cp -vf $rootpath"Doc/Font/文泉驿微米黑.ttf" "$destFont"
+	echo cp 文泉驿微米黑.ttf
+else
+	echo cp Dimbo.ttf
+	cp -vf $rootpath"Doc/Font/Dimbo Regular.ttf" "$destFont"
+fi
