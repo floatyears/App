4f2addac6296e0c3790e62b68195681a62de8d84
leiliang <303040565@qq.com>
2014-07-25 10:39:38 +0800

    no message

--- Assets/Scripts/View/UI/Common/ItemCounterController.cs
+++ Assets/Scripts/View/UI/Common/ItemCounterController.cs
@@ -5,15 +5,15 @@ public class ItemCounterController : ConcreteComponent{
 	public ItemCounterController(string name) : base(name){}
 
 	public override void ShowUI () {
-		Debug.LogError("ItemCounterController showui 1 ");
+//		Debug.LogError("ItemCounterController showui 1 ");
 		base.ShowUI ();
-		Debug.LogError("ItemCounterController showui 2 ");
+//		Debug.LogError("ItemCounterController showui 2 ");
 	}
 
 	public override void HideUI () {
-		Debug.LogError("ItemCounterController hide ui 1");
+//		Debug.LogError("ItemCounterController hide ui 1");
 		base.HideUI ();
-		Debug.LogError("ItemCounterController hide ui 2");
+//		Debug.LogError("ItemCounterController hide ui 2");
 //		base.DestoryUI ();
 	}
 }
