adf6315d644a744d46c68155d2e04ce04b863a4d
leiliang <303040565@qq.com>
2014-08-04 09:55:51 +0800

    no message

--- Assets/_Scripts/Utility/GameUtility.cs
+++ Assets/_Scripts/Utility/GameUtility.cs
@@ -1,8 +1,8 @@
-using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using System.IO;
+using UnityEngine;
 using bbproto;
 
 public class DGTools {
