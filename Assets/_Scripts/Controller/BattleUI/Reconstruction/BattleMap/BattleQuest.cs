94c919a14efa33155fae1ff948e15158a20eae66
leiliang <303040565@qq.com>
2014-08-08 15:20:04 +0800

    no message

--- Assets/_Scripts/Controller/BattleUI/Reconstruction/BattleMap/BattleQuest.cs
+++ Assets/_Scripts/Controller/BattleUI/Reconstruction/BattleMap/BattleQuest.cs
@@ -68,7 +68,7 @@ public class BattleQuest : UIBase {
 		viewManager.GetViewObject(backgroundName , o=>{
 			background = o as BattleBackground;
 			background.transform.parent = viewManager.BottomPanel.transform;
-			background.transform.localPosition = new Vector3(0f,20f,0f);
+			background.transform.localPosition = new Vector3(0f,25f,0f);
 			background.Init (backgroundName);
 			background.SetBattleQuest (this);
 			questData.questId = questDungeonData.QuestId;
