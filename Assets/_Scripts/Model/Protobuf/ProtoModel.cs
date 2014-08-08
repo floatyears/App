80ff064204e1f5864d542fced4031490c63442db
kory <119238@qq.com>
2014-08-08 15:21:07 +0800

    RspAcceptBonus

--- Assets/_Scripts/Model/Protobuf/ProtoModel.cs
+++ Assets/_Scripts/Model/Protobuf/ProtoModel.cs
@@ -6199,6 +6199,37 @@ namespace bbproto
       get { return _header; }
       set { _header = value; }
     }
+    private int _stone = default(int);
+    [global::ProtoBuf.ProtoMember(2, IsRequired = false, Name=@"stone", DataFormat = global::ProtoBuf.DataFormat.TwosComplement)]
+    [global::System.ComponentModel.DefaultValue(default(int))]
+    public int stone
+    {
+      get { return _stone; }
+      set { _stone = value; }
+    }
+    private int _money = default(int);
+    [global::ProtoBuf.ProtoMember(3, IsRequired = false, Name=@"money", DataFormat = global::ProtoBuf.DataFormat.TwosComplement)]
+    [global::System.ComponentModel.DefaultValue(default(int))]
+    public int money
+    {
+      get { return _money; }
+      set { _money = value; }
+    }
+    private int _friendPoint = default(int);
+    [global::ProtoBuf.ProtoMember(4, IsRequired = false, Name=@"friendPoint", DataFormat = global::ProtoBuf.DataFormat.TwosComplement)]
+    [global::System.ComponentModel.DefaultValue(default(int))]
+    public int friendPoint
+    {
+      get { return _friendPoint; }
+      set { _friendPoint = value; }
+    }
+    private readonly global::System.Collections.Generic.List<bbproto.UserUnit> _newUnitList = new global::System.Collections.Generic.List<bbproto.UserUnit>();
+    [global::ProtoBuf.ProtoMember(5, Name=@"newUnitList", DataFormat = global::ProtoBuf.DataFormat.Default)]
+    public global::System.Collections.Generic.List<bbproto.UserUnit> newUnitList
+    {
+      get { return _newUnitList; }
+    }
+  
     private global::ProtoBuf.IExtension extensionObject;
     global::ProtoBuf.IExtension global::ProtoBuf.IExtensible.GetExtensionObject(bool createIfMissing)
       { return global::ProtoBuf.Extensible.GetExtensionObject(ref extensionObject, createIfMissing); }
