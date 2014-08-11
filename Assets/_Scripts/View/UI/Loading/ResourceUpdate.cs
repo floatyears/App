ea02586973b409c3369606a110b88df5ef116050
kory <119238@qq.com>
2014-08-11 14:00:15 +0800

    no message

--- Assets/_Scripts/View/UI/Loading/ResourceUpdate.cs
+++ Assets/_Scripts/View/UI/Loading/ResourceUpdate.cs
@@ -88,7 +88,7 @@ public class ResourceUpdate : MonoBehaviour {
 #if LANGUAGE_CN
 	"版本：";
 #elif LANGUAGE_EN
-	"AppVersion:";
+	"Version:";
 #else
 	"";
 #endif
@@ -192,7 +192,7 @@ public class ResourceUpdate : MonoBehaviour {
 		for (int i = downLoadItemList.Count - 1; i >= 0; i--) {
 			DownloadItemInfo item = downLoadItemList[i];
 			if(!string.IsNullOrEmpty( item.www.error) /*&& item.retryCount <=0*/){
-				Debug.Log("download error");
+				Debug.Log("download error: "+item.www.error + " url:"+item.www.url);
 				downLoadItemList.Remove(item);
 				retryItemList.Add(item);
 			}else if(item.www.isDone) {
@@ -401,7 +401,7 @@ public class ResourceUpdate : MonoBehaviour {
 		//check the MD5, if not mamtch ,reload the file
 		if (serverVersionDic [downloadItem.name].md5 == hash) {
 			try{
-				Debug.Log("md5 is ok.  local res path: " + localResFullPath);
+				Debug.Log("md5 is ok.  local res path: " + localResFullPath +" downloadItem.name:"+downloadItem.name);
 				//File.WriteAllBytes (localResPath + "/" + serverVersionDic [name] [0] + ".unity3d", resBytes);
 				//only for test
 				File.WriteAllBytes ( localResFullPath + downloadItem.name + ".unity3d",downloadItem.www.bytes);
