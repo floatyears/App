9272a1fc6f100552e3dd148fdc7e05e1eba6f8f0
leiliang <303040565@qq.com>
2014-09-02 12:04:15 +0800

    no message

--- Assets/_Scripts/View/Effect/DragSliderBase.cs
+++ Assets/_Scripts/View/Effect/DragSliderBase.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class DragSliderBase : MonoBehaviour {
-	protected Transform leftTrans;
+	protected Transform moveParent = null;
+	protected Transform cacheLeftParent = null;
+	protected Transform cacheRightParent = null;
+	protected DragChangeViewSpring spring;
+	protected BoxCollider dragCollider;
+	protected Vector3 startPos;
+	protected Vector3 rightStartPos;
+	protected Vector3 leftStartPos;
+	protected IDragChangeView dragChangeViewData;
+	protected Vector3 intervDistance;
+	protected int changeDistance = 0;
+	protected UIPanel panel = null;
+	protected bool moveToRight;
+	protected bool stopOperate = false;
 
-	protected Transform rightTrans;
-
-	protected Transform centerTrans;
-
+	public virtual void Init() {
+		panel = GetComponent<UIPanel> ();
+		if (panel == null) {
+			panel = gameObject.AddComponent<UIPanel>();	
+		}
+	}
 }
