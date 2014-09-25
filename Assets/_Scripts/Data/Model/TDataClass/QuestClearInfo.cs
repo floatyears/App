34d3895c4e41c183245b4b642e6c861d1c31ba6d
floatyears <cl735915647@gmail.com>
2014-09-25 22:21:58 +0800

    战斗重构完毕

--- Assets/_Scripts/Data/Model/TDataClass/QuestClearInfo.cs
+++ Assets/_Scripts/Data/Model/TDataClass/QuestClearInfo.cs
@@ -15,157 +15,160 @@ public enum StageState {
 namespace bbproto{
 
 
-public partial class QuestClearInfo : ProtobufDataBase {
+	public partial class QuestClearInfo : ProtobufDataBase {
 
-    //// property ////
-	public	StageClearItem			StoryClear { get { return this.storyClear; } }
-	public	List<StageClearItem>	EventClear { get { return this.eventClear; } }
+	    //// property ////
+		public	StageClearItem			StoryClear { get { return this.storyClear; } }
+		public	List<StageClearItem>	EventClear { get { return this.eventClear; } }
 
-	public uint prevStageId(uint stageId) {
-		uint cityId = stageId / 10;
-		if ( stageId % 10 == 1 ) { // is frist stage of city
-			if (cityId==1) { //first city
-				return 0;
+		public uint prevStageId(uint stageId) {
+			uint cityId = stageId / 10;
+			if ( stageId % 10 == 1 ) { // is frist stage of city
+				if (cityId==1) { //first city
+					return 0;
+				}
+				//get prev city's last stage
+				CityInfo cityinfo = DataCenter.Instance.QuestData.GetCityInfo(cityId-1);
+				return cityinfo.stages[cityinfo.stages.Count-1].id;
 			}
-			//get prev city's last stage
-			CityInfo cityinfo = DataCenter.Instance.QuestData.GetCityInfo(cityId-1);
-			return cityinfo.stages[cityinfo.stages.Count-1].id;
+
+			return stageId-1;
 		}
-
-		return stageId-1;
-	}
-
-	//return 0:locked  1:cleared 2: currentOpen
-	public StageState GetStoryCityState(uint cityId) {
-		CityInfo cityinfo = DataCenter.Instance.QuestData.GetCityInfo(cityId);
-		bool isClear = true;
-			foreach( StageInfo stage in cityinfo.stages ) {
-			if (!IsStoryStageClear(stage)){
-				isClear = false;
-				break;
-			}
-		}
-		if(isClear) 
-			return StageState.CLEAR;
-
-		if(!isClear && cityId==1) 
-			return StageState.NEW;
-
-		//curr cityId is not clear, check prevCity
-		CityInfo prevCity = DataCenter.Instance.QuestData.GetCityInfo(cityId-1);
-		bool prevIsClear = true;
-			foreach( StageInfo stage in prevCity.stages ) {
-			if (!IsStoryStageClear(stage)){
-				prevIsClear = false;
-				break;
-			}
-		}
-		if(prevIsClear) return StageState.NEW;
-
-		return StageState.LOCKED;
-
-	}
 
 		//return 0:locked  1:cleared 2: currentOpen
-	public StageState GetStoryStageState(uint stageId) {
-		StageInfo stageinfo = DataCenter.Instance.QuestData.GetStageInfo(stageId);
+		public StageState GetStoryCityState(uint cityId) {
+			CityInfo cityinfo = DataCenter.Instance.QuestData.GetCityInfo(cityId);
+			bool isClear = true;
+				foreach( StageInfo stage in cityinfo.stages ) {
+				if (!IsStoryStageClear(stage)){
+					isClear = false;
+					break;
+				}
+			}
+			if(isClear) 
+				return StageState.CLEAR;
 
-		bool isClear = IsStoryStageClear(stageinfo);
-		if ( isClear ) {
-			return StageState.CLEAR;
+			if(!isClear && cityId==1) 
+				return StageState.NEW;
+
+			//curr cityId is not clear, check prevCity
+			CityInfo prevCity = DataCenter.Instance.QuestData.GetCityInfo(cityId-1);
+			bool prevIsClear = true;
+				foreach( StageInfo stage in prevCity.stages ) {
+				if (!IsStoryStageClear(stage)){
+					prevIsClear = false;
+					break;
+				}
+			}
+			if(prevIsClear) return StageState.NEW;
+
+			return StageState.LOCKED;
+
 		}
 
-		// current isClear==false, but previous stage is Cleared.
-		uint prevStage = prevStageId(stageId);
-		if (prevStage == 0 || IsStoryStageClear( DataCenter.Instance.QuestData.GetStageInfo(prevStage) ) )
-			return StageState.NEW;
+			//return 0:locked  1:cleared 2: currentOpen
+		public StageState GetStoryStageState(uint stageId) {
+			StageInfo stageinfo = DataCenter.Instance.QuestData.GetStageInfo(stageId);
 
-		return StageState.LOCKED;
-	}
+			bool isClear = IsStoryStageClear(stageinfo);
+			if ( isClear ) {
+				return StageState.CLEAR;
+			}
 
-	public	bool IsStoryStageClear(StageInfo stageInfo) {
-		if (StoryClear == null || stageInfo == null) {
-			return false;
+			// current isClear==false, but previous stage is Cleared.
+			uint prevStage = prevStageId(stageId);
+			if (prevStage == 0 || IsStoryStageClear( DataCenter.Instance.QuestData.GetStageInfo(prevStage) ) )
+				return StageState.NEW;
+
+			return StageState.LOCKED;
 		}
 
-		if (stageInfo.id == StoryClear.stageId) {
-			//Last quest of stage is clear, so the stage is clear.
-			return ( StoryClear.questId >= stageInfo.QuestInfo[stageInfo.QuestInfo.Count-1].id );
-		}
+		public	bool IsStoryStageClear(StageInfo stageInfo) {
+			if (StoryClear == null || stageInfo == null) {
+				return false;
+			}
 
-		return ( stageInfo.id < StoryClear.stageId );
-	}
-
-	public	bool IsEventStageClear(StageInfo stageInfo) {
-		if (EventClear == null) {
-			return false;
-		}
-
-		foreach(StageClearItem item in this.eventClear) {
-			if ( item.stageId == stageInfo.id ) { 
+			if (stageInfo.id == StoryClear.stageId) {
 				//Last quest of stage is clear, so the stage is clear.
-				bool isClear = ( item.questId == stageInfo.QuestInfo[stageInfo.QuestInfo.Count-1].id );
-				return isClear;
+				return ( StoryClear.questId >= stageInfo.QuestInfo[stageInfo.QuestInfo.Count-1].id );
 			}
-		}
-		
-		return false;
-	}
 
-	public	bool IsStoryQuestClear(uint stageId, uint questId) {
-		if (StoryClear == null) {
-			return false;
-		}
-		if ( stageId < StoryClear.stageId ) { 
-			return true;
-		} else if ( stageId == StoryClear.stageId ) { 
-			return ( questId <= StoryClear.questId );
+			return ( stageInfo.id < StoryClear.stageId );
 		}
 
-		return false;
-	}
-
-	public	bool IsEventQuestClear(uint stageId, uint questId) {
-		if (this.eventClear == null) {
-			return false;
-		}
-
-		foreach(StageClearItem item in this.eventClear) {
-			if ( item.stageId == stageId ) { 
-				return ( questId <= item.questId);
+		public	bool IsEventStageClear(StageInfo stageInfo) {
+			if (EventClear == null) {
+				return false;
 			}
-		}
 
-		return false;
-	}
-
-	public	void UpdateStoryQuestClear(uint stageId, uint questId) {
-		if( stageId > storyClear.stageId ) {
-			storyClear.stageId = stageId;
-		}
+			foreach(StageClearItem item in this.eventClear) {
+				if ( item.stageId == stageInfo.id ) { 
+					//Last quest of stage is clear, so the stage is clear.
+					bool isClear = ( item.questId == stageInfo.QuestInfo[stageInfo.QuestInfo.Count-1].id );
+					return isClear;
+				}
+			}
 			
-		if ( questId > storyClear.questId ) {
-			storyClear.questId = questId;
+			return false;
 		}
-		if (stageId >= 17 && questId >= 175) {
-			GameDataPersistence.Instance.StoreData("ResrouceDownload","Start");
-		}
-	}
 
-	public	void UpdateEventQuestClear(uint stageId, uint questId) {
-		foreach(StageClearItem item in this.eventClear) {
-			if ( item.stageId == stageId ) { //found exists stageId, update the lastest cleared questId.
-				if (questId > item.questId)
-					item.questId = questId;
-				return;
+		public	bool IsStoryQuestClear(uint stageId, uint questId) {
+			if (StoryClear == null) {
+				return false;
+			}
+			if ( stageId < StoryClear.stageId ) { 
+				return true;
+			} else if ( stageId == StoryClear.stageId ) { 
+				return ( questId <= StoryClear.questId );
+			}
+
+			return false;
+		}
+
+		public	bool IsEventQuestClear(uint stageId, uint questId) {
+			if (this.eventClear == null) {
+				return false;
+			}
+
+			foreach(StageClearItem item in this.eventClear) {
+				if ( item.stageId == stageId ) { 
+					return ( questId <= item.questId);
+				}
+			}
+
+			return false;
+		}
+
+		public	void UpdateStoryQuestClear(uint stageId, uint questId) {
+				if (storyClear == null) {
+					storyClear = new StageClearItem();			
+				}
+			if( stageId > storyClear.stageId ) {
+				storyClear.stageId = stageId;
+			}
+				
+			if ( questId > storyClear.questId ) {
+				storyClear.questId = questId;
+			}
+			if (stageId >= 17 && questId >= 175) {
+				GameDataPersistence.Instance.StoreData("ResrouceDownload","Start");
 			}
 		}
 
-		// not found old stageId, so create new one & append it.
-		StageClearItem sci = new StageClearItem();
-		sci.stageId = stageId;
-		sci.questId = questId;
-		this.eventClear.Add(sci);
+		public	void UpdateEventQuestClear(uint stageId, uint questId) {
+			foreach(StageClearItem item in this.eventClear) {
+				if ( item.stageId == stageId ) { //found exists stageId, update the lastest cleared questId.
+					if (questId > item.questId)
+						item.questId = questId;
+					return;
+				}
+			}
+
+			// not found old stageId, so create new one & append it.
+			StageClearItem sci = new StageClearItem();
+			sci.stageId = stageId;
+			sci.questId = questId;
+			this.eventClear.Add(sci);
+		}
 	}
-}
 }
