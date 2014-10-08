39dfec3ad9a7ab0411aa45f097c47ac69a54d84a
floatyears <cl735915647@gmail.com>
2014-10-08 14:08:35 +0800

    task & achieve

---
+++ Assets/_Scripts/Module/Achieve/AchieveModule.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchieveModule : ModuleBase {
+
+	public AchieveModule(UIConfigItem config):base(config){
+		CreateUI<AchieveView> ();
+	}
+}
---
+++ Assets/_Scripts/Module/Task/TaskView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskView : ViewBase {
+
+	public override void Init (UIConfigItem uiconfig, System.Collections.Generic.Dictionary<string, object> data)
+	{
+		base.Init (uiconfig, data);
+
+
+	}
+}
